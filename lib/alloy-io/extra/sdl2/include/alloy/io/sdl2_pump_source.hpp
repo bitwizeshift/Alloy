fe24009ab44ea6b98862e072eec40a17e81454d0
Matthew Rodusek <matthew.rodusek@gmail.com>
2019-05-17 21:54:03 -0400

    Add sdl2_pump_source
    This pump source pushes events from the SDL2 message pump.

---
+++ lib/alloy-io/extra/sdl2/include/alloy/io/sdl2_pump_source.hpp
@@ -0,0 +1,143 @@
+/*****************************************************************************
+ * \file sdl2_pump_source.hpp
+ *
+ * \brief
+ *****************************************************************************/
+
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2019 Matthew Rodusek All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+#ifndef ALLOY_IO_SDL2_GL_WINDOW_HPP
+#define ALLOY_IO_SDL2_GL_WINDOW_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include "alloy/io/message_pump.hpp"
+#include "alloy/core/utilities/not_null.hpp"
+
+#include <SDL2/SDL.h>
+#include <map>
+
+namespace alloy::io {
+
+  class sdl2_window;
+
+  //////////////////////////////////////////////////////////////////////////////
+  /// \brief The SDL2 message pump, as a message_pump::source
+  ///
+  /// Since the SDL pump is implemented as a static, global-modifying function,
+  /// it logically has exactly one instance. As a result, this message pump
+  /// is implemented as a singleton, and cannot be copied or moved.
+  ///
+  /// This pump source may emit any of the following events:
+  /// - window_show_event
+  /// - window_hide_event
+  /// - window_move_event
+  /// - window_resize_event
+  /// - window_minimize_event
+  /// - window_maximize_event
+  //////////////////////////////////////////////////////////////////////////////
+  class sdl2_pump_source final
+    : public message_pump::source
+  {
+    //--------------------------------------------------------------------------
+    // Static Functions
+    //--------------------------------------------------------------------------
+  public:
+
+    /// \brief Gets the static instance
+    ///
+    /// \return a reference to the static pump source
+    static sdl2_pump_source& instance();
+
+    //--------------------------------------------------------------------------
+    // Modifiers
+    //--------------------------------------------------------------------------
+  public:
+
+    /// \brief Attaches an sdl2_window to this pump source
+    ///
+    /// By attaching a window, window events may be propagated when messages
+    /// are pumped.
+    ///
+    /// \param window the window to attach
+    void attach_window( core::not_null<sdl2_window*> window );
+
+    /// \brief Detaches an sdl2_window from this pump source
+    ///
+    /// Removed windows have their events discarded during pumping.
+    ///
+    /// \param window the window to attach
+    void detach_window( core::not_null<sdl2_window*> window );
+
+    //--------------------------------------------------------------------------
+    // Private Constructors / Assignment
+    //--------------------------------------------------------------------------
+  private:
+
+    sdl2_pump_source() noexcept = default;
+    sdl2_pump_source( sdl2_pump_source&& other ) = delete;
+    sdl2_pump_source( const sdl2_pump_source& other ) = delete;
+
+    //--------------------------------------------------------------------------
+
+    sdl2_pump_source& operator=( sdl2_pump_source&& other ) = delete;
+    sdl2_pump_source& operator=( const sdl2_pump_source& other ) = delete;
+
+    //--------------------------------------------------------------------------
+    // Hooks
+    //--------------------------------------------------------------------------
+  private:
+
+    /// \brief Virtual hook for handling the message pump
+    ///
+    /// \note When pumping messages from SDL, any message created for a device
+    ///       that is not attached to this pump source will be lost. For
+    ///       example, if a window event occurs but is not attached to this
+    ///       pump, the event will not propagate.
+    ///
+    /// \param p the message_pump to handle the message
+    void pump(message_pump& p) noexcept override;
+
+    /// \brief Handles a window event
+    ///
+    /// \param event the SDL_Event to handle
+    /// \param p the message pump to pump events back into
+    void handle_window_event( const ::SDL_Event& event,
+                              message_pump& p ) noexcept;
+
+    //--------------------------------------------------------------------------
+    // Private Members
+    //--------------------------------------------------------------------------
+  private:
+
+    // TODO(bitwizeshift): Determine better allocation strategy for managing N
+    //                     windows.
+    std::map<unsigned,sdl2_window*> m_windows;
+  };
+
+} // namespace alloy::io
+
+#endif /* ALLOY_IO_SDL2_GL_WINDOW_HPP */
---
+++ lib/alloy-io/extra/sdl2/src/alloy/io/sdl2_pump_source.cpp
@@ -0,0 +1,116 @@
+#include "alloy/io/sdl2_pump_source.hpp"
+#include "alloy/io/sdl2_window.hpp"
+
+#include <SDL2/SDL.h>
+
+//------------------------------------------------------------------------------
+// Static Functions
+//------------------------------------------------------------------------------
+
+alloy::io::sdl2_pump_source& alloy::io::sdl2_pump_source::instance()
+{
+  static auto s_instance = sdl2_pump_source{};
+
+  return s_instance;
+};
+
+//------------------------------------------------------------------------------
+// Modifiers
+//------------------------------------------------------------------------------
+
+void alloy::io::sdl2_pump_source
+  ::attach_window( core::not_null<sdl2_window*> window )
+{
+  const auto id = ::SDL_GetWindowID(window->window_handle());
+  m_windows[static_cast<unsigned>(id)] = window.get();
+}
+
+void alloy::io::sdl2_pump_source
+  ::detach_window( core::not_null<sdl2_window*> window )
+{
+  const auto id = ::SDL_GetWindowID(window->window_handle());
+  const auto it = m_windows.find(static_cast<unsigned>(id));
+
+  ALLOY_ASSERT( it != m_windows.end() );
+
+  m_windows.erase(it);
+}
+
+//------------------------------------------------------------------------------
+// Hooks
+//------------------------------------------------------------------------------
+
+void alloy::io::sdl2_pump_source::pump(message_pump& p)
+  noexcept
+{
+  auto event = ::SDL_Event{};
+
+  // Poll all events and translate them
+  while (::SDL_PollEvent(&event)) {
+    if (event.type == SDL_WINDOWEVENT) {
+      handle_window_event(event, p);
+    }
+  }
+}
+
+void alloy::io::sdl2_pump_source
+  ::handle_window_event( const ::SDL_Event& event, message_pump& p )
+  noexcept
+{
+  const auto id = event.window.windowID;
+  const auto it = m_windows.find(id);
+
+  // Ignore this?
+  if (it == m_windows.end()) {
+    return;
+  }
+
+  const auto window = it->second;
+
+  switch (event.window.event) {
+    case SDL_WINDOWEVENT_SHOWN: {
+      p.post_event( window_show_event{window} );
+      break;
+    }
+    case SDL_WINDOWEVENT_HIDDEN: {
+      p.post_event( window_hide_event{window} );
+      break;
+    }
+    case SDL_WINDOWEVENT_MOVED: {
+      const auto x = event.window.data1;
+      const auto y = event.window.data2;
+
+      p.post_event( window_move_event{window, x, y} );
+      break;
+    }
+    case SDL_WINDOWEVENT_RESIZED: {
+      const auto width = event.window.data1;
+      const auto height = event.window.data2;
+
+      p.post_event( window_move_event{window, width, height} );
+      break;
+    }
+    case SDL_WINDOWEVENT_SIZE_CHANGED: {
+      const auto width = event.window.data1;
+      const auto height = event.window.data2;
+
+      p.post_event( window_move_event{window, width, height} );
+      break;
+    }
+    case SDL_WINDOWEVENT_MINIMIZED: {
+      p.post_event( window_minimize_event{window} );
+      break;
+    }
+    case SDL_WINDOWEVENT_MAXIMIZED: {
+      p.post_event( window_maximize_event{window} );
+      break;
+    }
+    case SDL_WINDOWEVENT_CLOSE: {
+      // TODO: find and unbind window event
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+}
