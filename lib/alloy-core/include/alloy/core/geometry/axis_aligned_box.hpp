df4652aa274f5a1c751ee79ac86b60c042c5abe6
Matthew Rodusek <matthew.rodusek@gmail.com>
2019-05-21 21:13:05 -0400

    Add axis aligned box geometry type
    This adds the new geometry type, an axis-aligned bounding box. This is
    used for simple intersections.

---
+++ lib/alloy-core/include/alloy/core/geometry/axis_aligned_box.hpp
@@ -0,0 +1,348 @@
+/*****************************************************************************
+ * \file axis_aligned_box.hpp
+ *
+ * \todo description
+ *****************************************************************************/
+
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2019 Matthew Rodusek All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+#ifndef ALLOY_CORE_GEOMETRY_AXIS_ALIGNED_BOX_HPP
+#define ALLOY_CORE_GEOMETRY_AXIS_ALIGNED_BOX_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include "alloy/core/precision.hpp"
+#include "alloy/core/assert.hpp"
+#include "alloy/core/math/math.hpp"
+#include "alloy/core/math/vector/vector3.hpp"
+#include "alloy/core/geometry/point.hpp"
+#include "alloy/core/geometry/plane.hpp"
+
+#include <array> // std::array
+
+namespace alloy::core {
+
+  //////////////////////////////////////////////////////////////////////////////
+  /// \brief A representation of a box that is aligned with the x, y, and z
+  ///        axes.
+  ///
+  /// Such a box is useful for making rough intersection calculations for an
+  /// initial check, before performing more in-depth intersection checks.
+  /// For example, a large model may contain one of these around the entire
+  /// model, and -- if an intersection occurs with this box -- a more
+  /// complicated intersection may be performed on all sub-parts of the model.
+  //////////////////////////////////////////////////////////////////////////////
+  class axis_aligned_box
+  {
+    //--------------------------------------------------------------------------
+    // Static Factory Functions
+    //--------------------------------------------------------------------------
+  public:
+
+    /// \brief Constructs an aligned box from a starting point, and a vector
+    ///        indicating the size
+    ///
+    /// \param p the point
+    /// \param size the vector that determines the x, y, z size
+    /// \return the created box
+    static axis_aligned_box from_point_and_size( const point& p,
+                                                 const vector3 size ) noexcept;
+
+    /// \brief Constructs an aligned box constrainted between the two points
+    ///        \p p0 and \p p1
+    ///
+    /// \note Unlike the constructor for this class, this factory does not have
+    ///       any preconditions on the points passed to this
+    ///
+    /// \param p0 the first point
+    /// \param p1 the second point
+    /// \return the created box
+    static axis_aligned_box from_points( const point& p0,
+                                         const point& p1 ) noexcept;
+
+    //--------------------------------------------------------------------------
+    // Constructors / Assignment
+    //--------------------------------------------------------------------------
+  public:
+
+    /// \brief Constructs an axis-aligned bounding-box from points from two
+    ///        opposite ends
+    ///
+    /// \pre \c lower_left.x()<=top_right.x()
+    /// \pre \c lower_left.y()<=top_right.y()
+    /// \pre \c lower_left.z()<=top_right.z()
+    ///
+    /// \param lower_left The first point
+    /// \param top_right The second point
+    axis_aligned_box( const point& lower_left,
+                      const point& top_right ) noexcept;
+
+    /// \brief Constructs an axis-aligned bounding-box by moving the contents
+    ///        from an existing one
+    ///
+    /// \param other the other box to move
+    axis_aligned_box( axis_aligned_box&& other ) noexcept = default;
+
+    /// \brief Constructs an axis-aligned bounding-box by copying the contents
+    ///        from an existing one
+    ///
+    /// \param other the other box to copy
+    axis_aligned_box( const axis_aligned_box& other ) noexcept = default;
+
+    //--------------------------------------------------------------------------
+
+    /// \brief Assigns the contents of an axis-aligned bounding-box to this one
+    ///
+    /// \param other the other box to move
+    /// \return reference to \c (*this)
+    axis_aligned_box& operator=( axis_aligned_box&& other ) noexcept = default;
+
+    /// \brief Assigns the contents of an axis-aligned bounding-box to this one
+    ///
+    /// \param other the other box to copy
+    /// \return reference to \c (*this)
+    axis_aligned_box& operator=( const axis_aligned_box& other ) noexcept = default;
+
+    //--------------------------------------------------------------------------
+    // Observers : Normals
+    //--------------------------------------------------------------------------
+  public:
+
+    /// \{
+    /// \brief Get the normal to the specified side
+    ///
+    /// \return the normal for the specified side
+    vector3 top_normal() const noexcept;
+    vector3 bottom_normal() const noexcept;
+    vector3 front_normal() const noexcept;
+    vector3 back_normal() const noexcept;
+    vector3 left_normal() const noexcept;
+    vector3 right_normal() const noexcept;
+    /// \}
+
+    //--------------------------------------------------------------------------
+    // Observers : Planes
+    //--------------------------------------------------------------------------
+  public:
+
+    /// \{
+    /// \brief Get the plane that defines the specified side
+    ///
+    /// \return the plane that define sthe specified side
+    plane top_plane() const noexcept;
+    plane bottom_plane() const noexcept;
+    plane front_plane() const noexcept;
+    plane back_plane() const noexcept;
+    plane left_plane() const noexcept;
+    plane right_plane() const noexcept;
+    /// \}
+
+    //--------------------------------------------------------------------------
+    // Observers : Points
+    //--------------------------------------------------------------------------
+  public:
+
+    /// \brief Gets the point at the bottom left of this box
+    ///
+    /// \return the point at the bottom-left of this box
+    point bottom_left_point() const noexcept;
+
+    /// \brief Gets the point at the top-right of this box
+    ///
+    /// \return the point at the top-right of this box
+    point top_right_point() const noexcept;
+
+    //--------------------------------------------------------------------------
+    // Observers
+    //--------------------------------------------------------------------------
+  public:
+
+    /// \brief Gets the area of this box
+    ///
+    /// \return the area
+    real area() const noexcept;
+
+    /// \brief Gets an array containing the 6 normals
+    ///
+    /// \return the normals
+    std::array<vector3,6> normals() const noexcept;
+
+    /// \brief Gets an array containing the 6 planes that encompass this box
+    ///
+    /// \return the planes
+    std::array<plane,6> planes() const noexcept;
+
+    /// \brief Gets the distance between this box and the specified \p point
+    ///
+    /// \param p the point to get the distance to
+    /// \return the distance
+    real distance_to( const point& p ) const noexcept;
+
+    /// \brief Checks if this box contains the given point \p p
+    ///
+    /// \param p the point to check for intersection
+    /// \return \c true if \p p is in the box
+    bool contains( const point& p ) const noexcept;
+
+    /// \brief Checks if this box contains the given point \p p
+    ///        relative to the given \p tolerance
+    ///
+    /// \param p the point to check for intersection
+    /// \param tolerance the tolerance for accepting the containment
+    /// \return \c true if \p p is in the box
+    bool contains( const point& p, real tolerance ) const noexcept;
+
+    /// \brief Checks if this box intersect another box
+    ///
+    /// \return \c true if this box intersects another box
+    bool intersects( const axis_aligned_box& other ) const noexcept;
+
+    /// \brief Checks if this box completely encloses another box
+    ///
+    /// \return \c true if this box completely encloses another box
+    bool encloses( const axis_aligned_box& other ) const noexcept;
+
+    //--------------------------------------------------------------------------
+    // Private Members
+    //--------------------------------------------------------------------------
+  private:
+
+    point m_bottom_left;
+    point m_top_right;
+  };
+
+  //============================================================================
+  // non-member functions : class : axis_aligned_box
+  //============================================================================
+
+  //----------------------------------------------------------------------------
+  // Equality
+  //----------------------------------------------------------------------------
+
+  bool operator==(const axis_aligned_box& lhs,
+                  const axis_aligned_box& rhs) noexcept;
+  bool operator!=(const axis_aligned_box& lhs,
+                  const axis_aligned_box& rhs) noexcept;
+
+  //----------------------------------------------------------------------------
+
+  /// \brief Determines equality between two planes relative to
+  ///        \ref default_tolerance
+  ///
+  /// \param lhs the left plane
+  /// \param rhs the right plane
+  /// \return \c true if the two planes contain almost equal values
+  bool almost_equal(const axis_aligned_box& lhs,
+                    const axis_aligned_box& rhs) noexcept;
+
+  /// \brief Determines equality between two planes relative to \p tolerance
+  ///
+  /// \param lhs the left plane
+  /// \param rhs the right plane
+  /// \param tolerance the tolerance for the comparison
+  /// \return \c true if the two planes contain almost equal values
+  bool almost_equal(const axis_aligned_box& lhs,
+                    const axis_aligned_box& rhs,
+                    real tolerance) noexcept;
+
+} // namespace alloy::core
+
+//==============================================================================
+// class : axis_aligned_box
+//==============================================================================
+
+//------------------------------------------------------------------------------
+// Constructors
+//------------------------------------------------------------------------------
+
+inline alloy::core::axis_aligned_box
+  ::axis_aligned_box( const point& lower_left, const point& top_right )
+  noexcept
+  : m_bottom_left{lower_left},
+    m_top_right{top_right}
+{
+  ALLOY_ASSERT( m_bottom_left.x() <= m_top_right.x() );
+  ALLOY_ASSERT( m_bottom_left.y() <= m_top_right.y() );
+  ALLOY_ASSERT( m_bottom_left.z() <= m_top_right.z() );
+}
+
+//------------------------------------------------------------------------------
+// Observers : Normals
+//------------------------------------------------------------------------------
+
+inline alloy::core::vector3 alloy::core::axis_aligned_box::top_normal()
+  const noexcept
+{
+  return top_plane().normal();
+}
+
+inline alloy::core::vector3 alloy::core::axis_aligned_box::bottom_normal()
+  const noexcept
+{
+  return bottom_plane().normal();
+}
+
+inline alloy::core::vector3 alloy::core::axis_aligned_box::front_normal()
+  const noexcept
+{
+  return front_plane().normal();
+}
+
+inline alloy::core::vector3 alloy::core::axis_aligned_box::back_normal()
+  const noexcept
+{
+  return back_plane().normal();
+}
+
+inline alloy::core::vector3 alloy::core::axis_aligned_box::left_normal()
+  const noexcept
+{
+  return left_plane().normal();
+}
+
+inline alloy::core::vector3 alloy::core::axis_aligned_box::right_normal()
+  const noexcept
+{
+  return right_plane().normal();
+}
+
+//------------------------------------------------------------------------------
+// Observers : Points
+//------------------------------------------------------------------------------
+
+inline alloy::core::point alloy::core::axis_aligned_box::bottom_left_point()
+  const noexcept
+{
+  return m_bottom_left;
+}
+
+inline alloy::core::point alloy::core::axis_aligned_box::top_right_point()
+  const noexcept
+{
+  return m_top_right;
+}
+
+#endif /* ALLOY_CORE_GEOMETRY_AXIS_ALIGNED_BOX_HPP */
---
+++ lib/alloy-core/src/alloy/core/geometry/axis_aligned_box.cpp
@@ -0,0 +1,229 @@
+#include "alloy/core/geometry/axis_aligned_box.hpp"
+
+#include <algorithm> // std::min, std::max
+
+//==============================================================================
+// class : axis_aligned_box
+//==============================================================================
+
+//------------------------------------------------------------------------------
+// Static Factories
+//------------------------------------------------------------------------------
+
+alloy::core::axis_aligned_box
+  alloy::core::axis_aligned_box::from_point_and_size( const point& p,
+                                                      const vector3 size )
+  noexcept
+{
+  return from_points(p, p + size);
+}
+
+alloy::core::axis_aligned_box
+  alloy::core::axis_aligned_box::from_points( const point& p0,
+                                              const point& p1 )
+  noexcept
+{
+  const auto min_point = point{
+    std::min(p0.x(),p1.x()),
+    std::min(p0.y(),p1.y()),
+    std::min(p0.z(),p1.z())
+  };
+  const auto max_point = point{
+    std::max(p0.x(),p1.x()),
+    std::max(p0.y(),p1.y()),
+    std::max(p0.z(),p1.z())
+  };
+  return axis_aligned_box{min_point, max_point};
+}
+
+//------------------------------------------------------------------------------
+// Observers : Planes
+//------------------------------------------------------------------------------
+
+alloy::core::plane alloy::core::axis_aligned_box::top_plane()
+  const noexcept
+{
+  const auto normal = vector3{0, 1, 0};
+  const auto distance = m_top_right.y();
+
+  return plane{normal, distance};
+}
+
+alloy::core::plane alloy::core::axis_aligned_box::bottom_plane()
+  const noexcept
+{
+  const auto normal = vector3{0, -1, 0};
+  const auto distance = m_bottom_left.y();
+
+  return plane{normal, distance};
+}
+
+alloy::core::plane alloy::core::axis_aligned_box::front_plane()
+  const noexcept
+{
+  const auto normal = vector3{0, 0, 1};
+  const auto distance = m_top_right.z();
+
+  return plane{normal, distance};
+}
+
+alloy::core::plane alloy::core::axis_aligned_box::back_plane()
+  const noexcept
+{
+  const auto normal = vector3{0, 0, -1};
+  const auto distance = m_bottom_left.z();
+
+  return plane{normal, distance};
+}
+
+alloy::core::plane alloy::core::axis_aligned_box::left_plane()
+  const noexcept
+{
+  const auto normal = vector3{-1, 0, 0};
+  const auto distance = m_bottom_left.x();
+
+  return plane{normal, distance};
+}
+
+alloy::core::plane alloy::core::axis_aligned_box::right_plane()
+  const noexcept
+{
+  const auto normal = vector3{1, 0, 0};
+  const auto distance = m_top_right.x();
+
+  return plane{normal, distance};
+}
+
+//------------------------------------------------------------------------------
+// Observers
+//------------------------------------------------------------------------------
+
+std::array<alloy::core::vector3,6> alloy::core::axis_aligned_box::normals()
+  const noexcept
+{
+  const auto p = planes();
+  auto result = std::array<vector3,6>{};
+
+  for (auto i = 0u; i < result.size(); ++i) {
+    result[i] = p[i].normal();
+  }
+
+  return result;
+}
+
+std::array<alloy::core::plane,6> alloy::core::axis_aligned_box::planes()
+  const noexcept
+{
+  return {
+    top_plane(),
+    bottom_plane(),
+    front_plane(),
+    back_plane(),
+    left_plane(),
+    right_plane()
+  };
+}
+
+bool alloy::core::axis_aligned_box::contains( const point& p )
+  const noexcept
+{
+  return (p.x() <= m_top_right.x()) && (p.x() >= m_bottom_left.x()) &&
+         (p.y() <= m_top_right.y()) && (p.y() >= m_bottom_left.y()) &&
+         (p.z() <= m_top_right.z()) && (p.z() >= m_bottom_left.z());
+}
+
+bool alloy::core::axis_aligned_box::contains( const point& p, real tolerance )
+  const noexcept
+{
+  // Add a buffer of 'tolerance' for fuzzy matching
+  return (p.x() <= (m_top_right.x() + tolerance)) &&
+         (p.x() >= (m_bottom_left.x() - tolerance)) &&
+         (p.y() <= (m_top_right.y() + tolerance)) &&
+         (p.y() >= (m_bottom_left.y() - tolerance)) &&
+         (p.z() <= (m_top_right.z() + tolerance)) &&
+         (p.z() >= (m_bottom_left.z() - tolerance));
+}
+
+bool alloy::core::axis_aligned_box::intersects( const axis_aligned_box& other )
+  const noexcept
+{
+  if (m_bottom_left.x() > other.m_top_right.x()) {
+    return false;
+  }
+  if (m_top_right.x() < other.m_bottom_left.x()) {
+    return false;
+  }
+
+  if (m_bottom_left.y() > other.m_top_right.y()) {
+    return false;
+  }
+  if (m_top_right.y() < other.m_top_right.y()) {
+    return false;
+  }
+
+  if (m_bottom_left.z() > other.m_top_right.z()) {
+    return false;
+  }
+  if (m_top_right.z() < other.m_bottom_left.z()) {
+    return false;
+  }
+
+  return true;
+}
+
+bool alloy::core::axis_aligned_box::encloses( const axis_aligned_box& other )
+  const noexcept
+{
+  return (m_bottom_left.x() <= other.m_bottom_left.x()) &&
+         (m_top_right.x() >= other.m_top_right.x()) &&
+         (m_bottom_left.y() <= other.m_bottom_left.y()) &&
+         (m_top_right.y() >= other.m_top_right.y()) &&
+         (m_bottom_left.z() <= other.m_bottom_left.z()) &&
+         (m_top_right.z() >= other.m_top_right.z());
+}
+
+//==============================================================================
+// non-member functions : class : axis_aligned_box
+//==============================================================================
+
+//------------------------------------------------------------------------------
+// Equality
+//------------------------------------------------------------------------------
+
+bool alloy::core::operator==( const axis_aligned_box& lhs,
+                              const axis_aligned_box& rhs )
+  noexcept
+{
+  return lhs.top_right_point() == rhs.top_right_point() &&
+         lhs.bottom_left_point() == rhs.bottom_left_point();
+}
+
+bool alloy::core::operator!=( const axis_aligned_box& lhs,
+                              const axis_aligned_box& rhs )
+  noexcept
+{
+  return !(lhs==rhs);
+}
+
+//------------------------------------------------------------------------------
+
+bool alloy::core::almost_equal( const axis_aligned_box& lhs,
+                                const axis_aligned_box& rhs )
+  noexcept
+{
+  return almost_equal(lhs.top_right_point(), rhs.top_right_point()) &&
+         almost_equal(lhs.bottom_left_point(), rhs.bottom_left_point());
+}
+
+bool alloy::core::almost_equal( const axis_aligned_box& lhs,
+                                const axis_aligned_box& rhs,
+                                real tolerance )
+  noexcept
+{
+  return almost_equal(lhs.top_right_point(),
+                      rhs.top_right_point(),
+                      tolerance) &&
+         almost_equal(lhs.bottom_left_point(),
+                      rhs.bottom_left_point(),
+                      tolerance);
+}
