0dbd04ff0bfdb9d7897b7860e254af61d5caef40
Matthew Rodusek <matthew.rodusek@gmail.com>
2019-06-02 14:42:55 -0400

    Fix broken use-discrete-gpu project
    The use-discrete-gpu project was broken by the removal of the
    API macro definitions from 'config.hpp'. Since these macros are
    defined on a per-library basis now, there is no simple header to
    include these macros.
    
    However this project produces a static library that never exports
    the symbol, and so the change was to outright define the macros
    as part of the source, rather than include from another source.

--- extra/use-discrete-gpu/src/use_discrete.cpp
+++ extra/use-discrete-gpu/src/use_discrete.cpp
@@ -1,4 +1,8 @@
-#include "alloy/core/config.hpp"
+#if defined(WIN32) || defined(_WIN32)
+# define ALLOY_EXTRA_API_EXPORT __declspec(dllexport)
+#else
+# define ALLOY_EXTRA_API_EXPORT __attribute__ ((__visibility__("default")))
+#endif
 
 // Note: The 'extern' declaration and value definition have been separated into
 //       two separate statements in order to avoid errors when using -Wall due
@@ -11,7 +15,7 @@
 ///
 /// \note 'unsigned long' is used to avoid importing windows.h for a DWORD
 ///       which would not be cross-platform.
-extern "C" ALLOY_API_EXPORT unsigned long NvOptimusEnablement;
+extern "C" ALLOY_EXTRA_API_EXPORT unsigned long NvOptimusEnablement;
 unsigned long NvOptimusEnablement = 0x00000001;
 
 /// \brief A global variable indicating to devices with both embedded and
@@ -23,5 +27,5 @@ unsigned long NvOptimusEnablement = 0x00000001;
 /// \note The first reference lists 'DWORD' as the required underlying type, whereas
 ///       AMD lists 'int' as the underlying type. 'unsigned long' is used to avoid
 ///       an import of 'windows.h' for a DWORD, which would not be cross-platform.
-extern "C" ALLOY_API_EXPORT unsigned long AmdPowerXpressRequestHighPerformance;
+extern "C" ALLOY_EXTRA_API_EXPORT unsigned long AmdPowerXpressRequestHighPerformance;
 unsigned long AmdPowerXpressRequestHighPerformance = 1;
