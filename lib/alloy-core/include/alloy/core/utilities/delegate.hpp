c70a573bc244fd0714e56ca6c8708c6d3ab1c72b
Matthew Rodusek <matthew.rodusek@gmail.com>
2019-09-27 23:54:29 -0400

    Add lightweight delegate type
    This new type exists for rebinding raw function pointers, and pointers
    to member/member-functions in a simple, lightweight class.
    
    Due to the way this class operates, compilers like gcc and clang
    (presumably others like MSVC as well) are able to see into the
    types and optimize many of the indirect calls -- making this superior
    to std::function in most regards. The lack of heap allocations also
    simplifies things greatly.
    
    The only complication that occurs from this is that delegate requires
    a static template function to be invoked in order to produce a delegate
    type -- rather than binding easily in the constructor. This effectively
    takes 2 steps rather than std::function's 1 step in order to bind
    functionality -- which ultimately is a small price to pay.

---
+++ lib/alloy-core/include/alloy/core/utilities/delegate.hpp
@@ -0,0 +1,489 @@
+/*****************************************************************************
+ * \file delegate.hpp
+ *
+ * \brief This header defines a utility for managing function dispatch
+ *****************************************************************************/
+
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2019 Matthew Rodusek All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+#ifndef ALLOY_CORE_UTILITIES_DELEGATE_HPP
+#define ALLOY_CORE_UTILITIES_DELEGATE_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include "alloy/core/assert.hpp" // ALLOY_ASSERT
+#include "alloy/core/config.hpp" // ALLOY_CORE_EXCEPTIONS_ENABLE
+
+#include <type_traits> // std::true_type
+#include <functional>  // std::invoke
+#include <cstddef>     // std::nullptr_t
+
+#if ALLOY_CORE_EXCEPTIONS_ENABLED
+# include <exception> // std::exception
+#endif
+
+namespace alloy::core {
+
+  //===========================================================================
+  // class : bad_delegate_call
+  //===========================================================================
+
+#if ALLOY_CORE_EXCEPTIONS_ENABLED
+
+  /////////////////////////////////////////////////////////////////////////////
+  /// \brief An exception thrown when a delegate is invoked without a bound
+  ///        function
+  /////////////////////////////////////////////////////////////////////////////
+  class bad_delegate_call final : public std::exception
+  {
+    //-------------------------------------------------------------------------
+    // Constructors / Assignment
+    //-------------------------------------------------------------------------
+  public:
+
+    bad_delegate_call() noexcept = default;
+    bad_delegate_call(const bad_delegate_call&) noexcept = default;
+    bad_delegate_call(bad_delegate_call&&) noexcept = default;
+
+    bad_delegate_call& operator=(const bad_delegate_call&) = default;
+    bad_delegate_call& operator=(bad_delegate_call&&) = default;
+
+    //-------------------------------------------------------------------------
+    // Observers
+    //-------------------------------------------------------------------------
+  public:
+
+    const char* what() const noexcept override;
+
+  };
+
+#endif // ALLOY_CORE_EXCEPTIONS_ENABLED
+
+  //===========================================================================
+  // class : delegate
+  //===========================================================================
+
+  template <typename Fn>
+  class delegate;
+
+  /////////////////////////////////////////////////////////////////////////////
+  /// \brief A class for binding light-weight non-owning functions
+  ///
+  /// \tparam R the result type
+  /// \tparam Args the argument types
+  /////////////////////////////////////////////////////////////////////////////
+  template <typename R, typename...Args>
+  class delegate<R(Args...)>
+  {
+    template <typename F, typename...Ts>
+    using enable_if_invocable_t = std::enable_if_t<
+      std::conjunction_v<
+        std::is_invocable<F, Ts...>,
+        std::disjunction<
+          std::is_void<R>,
+          std::is_convertible<std::invoke_result_t<F, Ts...>,R>
+        >
+      >
+    >;
+
+    //-------------------------------------------------------------------------
+    // Public Member Types
+    //-------------------------------------------------------------------------
+  public:
+
+    using result_type = R;
+
+    //-------------------------------------------------------------------------
+    // Static Factories
+    //-------------------------------------------------------------------------
+
+    /// \brief Binds a non-member function pointer to this delegate
+    ///
+    /// \tparam Fn the function pointer to bind
+    /// \return the bound delegate
+    template <auto Fn,
+              typename=enable_if_invocable_t<decltype(Fn),Args...>>
+    static constexpr delegate bind() noexcept;
+
+    /// \brief Binds a member function pointer to this delegate
+    ///
+    /// \tparam MemberFn the member function pointer to bind
+    /// \return the bound delegate
+    template <auto MemberFn, typename C,
+              typename=enable_if_invocable_t<decltype(MemberFn),C*,Args...>>
+    static constexpr delegate bind(C* c) noexcept;
+
+    /// \brief Binds a const member function pointer to this delegate
+    ///
+    /// \tparam MemberFn the const member function pointer to bind
+    /// \return the bound delegate
+    template <auto MemberFn, typename C,
+              typename=enable_if_invocable_t<decltype(MemberFn),const C*,Args...>>
+    static constexpr delegate bind(const C* c) noexcept;
+
+    /// \{
+    /// \brief Binds a callable to this delegate
+    ///
+    /// \param callable the callable to bind
+    /// \return the bound delegate
+    template <typename Callable,
+              typename=enable_if_invocable_t<Callable&,Args...>>
+    static constexpr delegate bind(Callable* callable) noexcept;
+    template <typename Callable,
+              typename=enable_if_invocable_t<const Callable&,Args...>>
+    static constexpr delegate bind(const Callable* callable) noexcept;
+    /// \}
+
+    // Disallow binding nulls
+    template <auto MemberFn>
+    static delegate bind(std::nullptr_t) = delete;
+    static delegate bind(std::nullptr_t) = delete;
+
+    //-------------------------------------------------------------------------
+    // Constructors / Assignment
+    //-------------------------------------------------------------------------
+  public:
+
+    /// \brief Constructs a delegate that does not have a bound function
+    constexpr delegate() noexcept;
+    constexpr delegate(const delegate&) noexcept = default;
+    constexpr delegate(delegate&&) noexcept = default;
+
+    //-------------------------------------------------------------------------
+
+    delegate& operator=(const delegate&) noexcept = default;
+    delegate& operator=(delegate&&) noexcept = default;
+
+    //-------------------------------------------------------------------------
+    // Observers
+    //-------------------------------------------------------------------------
+  public:
+
+    /// \brief Invokes the underlying delegate with the specified \p args
+    ///
+    /// \param args the arguments
+    /// \return the result of the function
+    template <typename...UArgs,
+              typename=std::enable_if_t<std::is_invocable_v<R(*)(Args...),UArgs...>>>
+    R operator()(UArgs&&...args) const;
+
+    /// \brief Returns \c true if this delegate contains a function
+    constexpr explicit operator bool() const noexcept;
+
+    //-------------------------------------------------------------------------
+    // Private Member Types
+    //-------------------------------------------------------------------------
+  private:
+
+    using function_type = R(*)(const void*, Args...);
+
+    //-------------------------------------------------------------------------
+    // Private Constructors
+    //-------------------------------------------------------------------------
+  private:
+
+    constexpr delegate(const void* instance, function_type function) noexcept;
+
+    //-------------------------------------------------------------------------
+    // Private Member Types
+    //-------------------------------------------------------------------------
+  private:
+
+    const void*   m_instance;
+    function_type m_function;
+
+    template <typename R2, typename...Args2>
+    friend constexpr bool operator==(const delegate<R2(Args2...)>&,
+                                     const delegate<R2(Args2...)>&) noexcept;
+  };
+
+  //===========================================================================
+  // non-member functions : class : delegateR(Args...)>
+  //===========================================================================
+
+  //---------------------------------------------------------------------------
+  // Equality
+  //---------------------------------------------------------------------------
+
+  template <typename R, typename...Args>
+  constexpr bool operator==(const delegate<R(Args...)>& lhs,
+                            const delegate<R(Args...)>& rhs) noexcept;
+  template <typename R, typename...Args>
+  constexpr bool operator!=(const delegate<R(Args...)>& lhs,
+                            const delegate<R(Args...)>& rhs) noexcept;
+
+} // namespace alloy::core
+
+
+#if ALLOY_CORE_EXCEPTIONS_ENABLED
+
+//==============================================================================
+// inline definitions : class : bad_delegate_call
+//==============================================================================
+
+//------------------------------------------------------------------------------
+// Observers
+//------------------------------------------------------------------------------
+
+inline const char* alloy::core::bad_delegate_call::what()
+  const noexcept
+{
+  return "bad_delegate_call";
+}
+
+#endif // ALLOY_CORE_EXCEPTIONS_ENABLED
+
+//=============================================================================
+// inline definitions : class : delegate
+//=============================================================================
+
+namespace alloy::core::detail {
+
+  template <typename T>
+  using delegate_forward_result_t = std::conditional_t<
+    std::is_reference<T>::value, T, std::add_rvalue_reference_t<T>
+  >;
+
+  // A small utility for forwarding class template arguments in a variadic
+  // member function.
+  //
+  // This allow by-value move-only types to be propagates to the underlying
+  // function (e.g. for if std::unique_ptr is a parameter).
+  //
+  // * l-value references are passed-through as l-value references
+  // * r-value references are passed-through as r-value references
+  // * By-value arguments are std::moved to r-value references
+  template <typename T, typename U>
+  constexpr delegate_forward_result_t<T> delegate_forward(U&& u)
+  {
+    return static_cast<delegate_forward_result_t<T>>(u);
+  }
+
+} // namespace alloy::core::detail
+
+//-----------------------------------------------------------------------------
+// Static Factories
+//-----------------------------------------------------------------------------
+
+template <typename R, typename...Args>
+template <auto Fn, typename>
+inline constexpr alloy::core::delegate<R(Args...)>
+  alloy::core::delegate<R(Args...)>::bind()
+  noexcept
+{
+  const auto ptr = static_cast<const void*>(nullptr);
+  const auto fn  = [](const void* p, Args...args) -> R
+  {
+    (void) p;
+
+    if constexpr (std::is_void<R>::value) {
+      std::invoke(Fn, detail::delegate_forward<Args>(args)...);
+    } else {
+      return std::invoke(Fn, detail::delegate_forward<Args>(args)...);
+    }
+  };
+
+  return delegate{ptr, static_cast<function_type>(fn)};
+}
+
+
+template <typename R, typename...Args>
+template <auto MemberFn, typename C, typename>
+inline constexpr alloy::core::delegate<R(Args...)>
+  alloy::core::delegate<R(Args...)>::bind(C* c)
+  noexcept
+{
+  ALLOY_ASSERT(c != nullptr);
+
+  const auto ptr = static_cast<const void*>(c);
+  const auto fn  = [](const void* p, Args...args) -> R
+  {
+    auto* pc = static_cast<C*>(const_cast<void*>(p));
+
+    if constexpr (std::is_void<R>::value) {
+      std::invoke(MemberFn, pc, detail::delegate_forward<Args>(args)...);
+    } else {
+      return std::invoke(MemberFn, pc, detail::delegate_forward<Args>(args)...);
+    }
+  };
+
+  return delegate{ptr, static_cast<function_type>(fn)};
+}
+
+
+template <typename R, typename...Args>
+template <auto MemberFn, typename C, typename>
+inline constexpr alloy::core::delegate<R(Args...)>
+  alloy::core::delegate<R(Args...)>::bind(const C* c)
+  noexcept
+{
+  ALLOY_ASSERT(c != nullptr);
+
+  const auto ptr = static_cast<const void*>(c);
+  const auto fn  = [](const void* p, Args...args) -> R
+  {
+    auto* pc = static_cast<const C*>(p);
+
+    if constexpr (std::is_void<R>::value) {
+      std::invoke(MemberFn, pc, detail::delegate_forward<Args>(args)...);
+    } else {
+      return std::invoke(MemberFn, pc, detail::delegate_forward<Args>(args)...);
+    }
+  };
+
+  return delegate{ptr, static_cast<function_type>(fn)};
+}
+
+
+template <typename R, typename...Args>
+template <typename Callable, typename>
+inline constexpr alloy::core::delegate<R(Args...)>
+  alloy::core::delegate<R(Args...)>::bind(Callable* callable)
+  noexcept
+{
+  ALLOY_ASSERT(callable != nullptr);
+
+  const auto ptr = static_cast<const void*>(callable);
+  const auto fn  = [](const void* p, Args...args) -> R
+  {
+    auto* pc = static_cast<Callable*>(const_cast<void*>(p));
+
+    if constexpr (std::is_void<R>::value) {
+      std::invoke(*pc, detail::delegate_forward<Args>(args)...);
+    } else {
+      return std::invoke(*pc, detail::delegate_forward<Args>(args)...);
+    }
+  };
+
+  return delegate{ptr, static_cast<function_type>(fn)};
+}
+
+
+template <typename R, typename...Args>
+template <typename Callable, typename>
+inline constexpr alloy::core::delegate<R(Args...)>
+  alloy::core::delegate<R(Args...)>::bind(const Callable* callable)
+  noexcept
+{
+  ALLOY_ASSERT(callable != nullptr);
+
+  const auto ptr = static_cast<const void*>(callable);
+  const auto fn  = [](const void* p, Args...args) -> R
+  {
+    const auto* pc = static_cast<const Callable*>(p);
+
+    if constexpr (std::is_void<R>::value) {
+      std::invoke(*pc, detail::delegate_forward<Args>(args)...);
+    } else {
+      return std::invoke(*pc, detail::delegate_forward<Args>(args)...);
+    }
+  };
+
+  return delegate{ptr, static_cast<function_type>(fn)};
+}
+
+//-----------------------------------------------------------------------------
+// Constructors
+//-----------------------------------------------------------------------------
+
+template <typename R, typename...Args>
+inline constexpr alloy::core::delegate<R(Args...)>::delegate()
+  noexcept
+  : delegate{nullptr,nullptr}
+{
+
+}
+
+//-----------------------------------------------------------------------------
+// Observer
+//-----------------------------------------------------------------------------
+
+template <typename R, typename...Args>
+template <typename...UArgs, typename>
+inline R alloy::core::delegate<R(Args...)>::operator()(UArgs&&...args)
+  const
+{
+#if ALLOY_CORE_EXCEPTIONS_ENABLED
+  if (m_function == nullptr) {
+    throw bad_delegate_call{};
+  }
+#else
+  ALLOY_ASSERT(m_function != nullptr);
+#endif
+
+  return m_function(m_instance, std::forward<UArgs>(args)...);
+}
+
+
+template <typename R, typename...Args>
+inline constexpr alloy::core::delegate<R(Args...)>::operator bool()
+  const noexcept
+{
+  return m_function != nullptr;
+}
+
+//-----------------------------------------------------------------------------
+// Private Constructors
+//-----------------------------------------------------------------------------
+
+template <typename R, typename...Args>
+inline constexpr alloy::core::delegate<R(Args...)>
+  ::delegate(const void* instance, function_type function)
+  noexcept
+  : m_instance{instance},
+    m_function{function}
+{
+
+}
+
+//=============================================================================
+// inline definitions : non-member functions
+//=============================================================================
+
+//-----------------------------------------------------------------------------
+// Equality
+//-----------------------------------------------------------------------------
+
+template <typename R, typename...Args>
+inline constexpr bool
+  alloy::core::operator==(const delegate<R(Args...)>& lhs,
+                          const delegate<R(Args...)>& rhs)
+  noexcept
+{
+  return lhs.m_instance == rhs.m_instance &&
+         lhs.m_function == rhs.m_function;
+}
+
+template <typename R, typename...Args>
+inline constexpr bool
+  alloy::core::operator!=(const delegate<R(Args...)>& lhs,
+                          const delegate<R(Args...)>& rhs)
+  noexcept
+{
+  return !(lhs == rhs);
+}
+
+#endif /* ALLOY_CORE_UTILITIES_DELEGATE_HPP */
