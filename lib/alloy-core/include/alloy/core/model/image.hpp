eeb77bf72d1a7bc292cf5e50d508e336786d5ff1
Matthew Rodusek <matthew.rodusek@gmail.com>
2019-10-01 18:59:52 -0400

    Add image
    This adds a new semantic type, alloy::core::image which represents a
    2D graphical image, modeled as a byte-stream.
    
    This will be required for texture loading later on in the system.

---
+++ lib/alloy-core/include/alloy/core/model/image.hpp
@@ -0,0 +1,286 @@
+/*****************************************************************************
+ * \file image.hpp
+ *
+ * \todo description
+ *****************************************************************************/
+
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2019 Matthew Rodusek All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+#ifndef ALLOY_CORE_MODEL_IMAGE_HPP
+#define ALLOY_CORE_MODEL_IMAGE_HPP
+
+#include "alloy/core/model/color.hpp"
+#include "alloy/core/memory/allocator.hpp"
+#include "alloy/core/containers/vector.hpp"
+#include "alloy/core/utilities/expected.hpp"
+
+#include <cstdint>      // std::uint8_t
+#include <cstddef>      // std::size_t, std::byte
+#include <system_error> // std::error_code, std::is_error_code_enum
+#include <type_traits>  // std::true_type
+
+namespace alloy::core {
+
+
+  /////////////////////////////////////////////////////////////////////////////
+  /// \brief A general image abstraction
+  ///
+  /// An image is implemented as a contiguous array of bytes that represents
+  /// a 2D graphical image.
+  /////////////////////////////////////////////////////////////////////////////
+  class image
+  {
+    //-------------------------------------------------------------------------
+    // Public Member Types
+    //-------------------------------------------------------------------------
+  public:
+
+    ///////////////////////////////////////////////////////////////////////////
+    /// \brief The order that pixels are stored in this image
+    ///////////////////////////////////////////////////////////////////////////
+    enum class pixel_format : std::uint8_t {
+      rgb,
+      rgba,
+      bgr,
+      bgra,
+    };
+
+    struct dimensions {
+      std::size_t width;
+      std::size_t height;
+    };
+
+    enum class error_code {
+      invalid_pixel_format, ///< Pixel format is not correct
+      bad_width,            ///< Width specified as 0
+      bad_height,           ///< Height specified as 0
+      incorrect_dimensions, ///< Dimensions do not match buffer size
+    };
+
+    //--------------------------------------------------------------------------
+    // Static Factories
+    //--------------------------------------------------------------------------
+  public:
+
+    /// \brief Makes an image from a byte buffer
+    ///
+    /// \pre \p buffer must be of n * d.width * d.height size, where n is the
+    ///         number of bytes specified in the pixel_format
+    /// \pre \c d.width must be greater than 0
+    /// \pre \c d.height must be greater than 0
+    ///
+    /// \param buffer the underlying byte buffer
+    /// \param d the dimensions of the image
+    /// \param format the pixel format of the image
+    ///
+    /// \return an image on success, an error_code on failure
+    static expected<image> make_image_from_buffer(vector<std::byte> buffer,
+                                                  dimensions d,
+                                                  pixel_format format);
+
+    /// \{
+    /// \brief Makes an image with a solid color
+    ///
+    /// \pre \p buffer must be of n * d.width * d.height size, where n is the
+    ///         number of bytes specified in the pixel_format
+    /// \pre \c d.width must be greater than 0
+    /// \pre \c d.height must be greater than 0
+    ///
+    /// \param d the dimensions of the image
+    /// \param format the pixel format of the image
+    /// \param background the color to set the background of the image
+    /// \param alloc the allocator to use for constructing the image
+    ///
+    /// \return an image on success, an error_code on failure
+    static expected<image> make_blank_image(dimensions d,
+                                            pixel_format format,
+                                            color background);
+    static expected<image> make_blank_image(dimensions d,
+                                            pixel_format format,
+                                            color background,
+                                            allocator alloc);
+    /// \}
+
+    //--------------------------------------------------------------------------
+    // Constructors / Assignment
+    //--------------------------------------------------------------------------
+  public:
+
+    /// \brief Constructs an image of 0 size
+    image() noexcept;
+    image(image&& other) noexcept = default;
+    image(const image&) = delete;
+
+    //--------------------------------------------------------------------------
+
+    image& operator=(image&& other) noexcept = default;
+    image& operator=(const image&) = delete;
+
+    //--------------------------------------------------------------------------
+    // Modifiers
+    //--------------------------------------------------------------------------
+  public:
+
+    /// \brief Sets the pixel at the image coordinate (x,y)
+    ///
+    /// \pre \p x must be between [0, width())
+    /// \pre \p y must be between [0, height())
+    ///
+    /// \param x the x position
+    /// \param y the y position
+    /// \param c the color
+    void set_pixel(std::size_t x, std::size_t y, color c) noexcept;
+
+    //--------------------------------------------------------------------------
+    // Accessors
+    //--------------------------------------------------------------------------
+  public:
+
+    /// \brief Gets the pixel at the image coordinate (x,y)
+    ///
+    /// \pre \p x must be between [0, width())
+    /// \pre \p y must be between [0, height())
+    ///
+    /// \param x the x coordinate
+    /// \param y the y coordinate
+    /// \return the color at image coordinate (x,y)
+    color get_pixel(std::size_t x, std::size_t y) const noexcept;
+
+    //--------------------------------------------------------------------------
+    // Observers
+    //--------------------------------------------------------------------------
+  public:
+
+    /// \brief Returns a copy of this image
+    ///
+    /// \return the copy of the image
+    image copy() const;
+
+    /// \brief Returns a copy of this image allocated using \p alloc
+    ///
+    /// \param alloc the allocator to use
+    /// \return the new image
+    image copy(allocator alloc) const;
+
+    //--------------------------------------------------------------------------
+
+    /// \brief Returns a pointer to the underlying image data
+    ///
+    /// \return a pointer to the underlying image data
+    const std::byte* data() const noexcept;
+
+    /// \brief Gets the width of this image
+    ///
+    /// \return the width of this image
+    std::size_t width() const noexcept;
+
+    /// \brief Gets the height of this image
+    ///
+    /// \return the height of this image
+    std::size_t height() const noexcept;
+
+    /// \brief Gets the pixel format of this image
+    ///
+    /// \return the pixel format of this image
+    pixel_format format() const noexcept;
+
+    //--------------------------------------------------------------------------
+    // Private : Constructors
+    //--------------------------------------------------------------------------
+  private:
+
+    /// \brief Fully constructs the underlying image
+    image(vector<std::byte> bytes,
+          dimensions d,
+          pixel_format format) noexcept;
+
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+  private:
+
+    vector<std::byte> m_bytes;
+    std::size_t m_width;
+    std::size_t m_height;
+    pixel_format m_format;
+  };
+
+  //===========================================================================
+  // non-member functions : enum : image::error_code
+  //===========================================================================
+
+  //---------------------------------------------------------------------------
+  // Utilities
+  //---------------------------------------------------------------------------
+
+  /// \brief Makes an error code from an image error_code
+  ///
+  /// \param ec the error code
+  /// \return the error code
+  std::error_code make_error_code(image::error_code ec) noexcept;
+
+} // namespace alloy::core
+
+namespace std {
+  template <>
+  struct is_error_code_enum<::alloy::core::image::error_code>
+    : std::true_type{};
+} // namespace error_code
+
+//=============================================================================
+// inline definitions : class : image
+//=============================================================================
+
+//-----------------------------------------------------------------------------
+// Observers
+//-----------------------------------------------------------------------------
+
+inline const std::byte* alloy::core::image::data()
+  const noexcept
+{
+  return m_bytes.data();
+}
+
+
+inline std::size_t alloy::core::image::width()
+  const noexcept
+{
+  return m_width;
+}
+
+
+inline std::size_t alloy::core::image::height()
+  const noexcept
+{
+  return m_height;
+}
+
+
+inline alloy::core::image::pixel_format alloy::core::image::format()
+  const noexcept
+{
+  return m_format;
+}
+
+#endif /* ALLOY_CORE_MODEL_IMAGE_HPP */
---
+++ lib/alloy-core/src/alloy/core/model/image.cpp
@@ -0,0 +1,431 @@
+#include "alloy/core/model/image.hpp"
+
+#include "alloy/core/intrinsics.hpp"
+
+#include <utility> // std::move
+#include <string>
+
+namespace {
+
+  //===========================================================================
+  // class : error_category
+  //===========================================================================
+
+  /////////////////////////////////////////////////////////////////////////////
+  /// \brief Error category for the image::error_code enum
+  /////////////////////////////////////////////////////////////////////////////
+  class error_category final
+    : public std::error_category
+  {
+  public:
+    std::string message(int ec) const override
+    {
+      using std::literals::string_literals::operator""s;
+      using error_code = ::alloy::core::image::error_code;
+      auto code = static_cast<error_code>(ec);
+
+      switch (code) {
+        case error_code::invalid_pixel_format: {
+          return "Invalid pixel format value specified"s;
+        }
+        case error_code::bad_width: {
+          return "Invalid width specified. Width must be > 0"s;
+        }
+        case error_code::bad_height: {
+          return "Invalid height specified. Height must be > 0"s;
+        }
+        case error_code::incorrect_dimensions: {
+          return "Dimensions don't match the supplied byte buffer"s;
+        }
+        default: {
+          return "Unknown error code"s;
+        }
+      }
+    }
+
+    const char* name() const noexcept override
+    {
+      return "image error";
+    }
+  };
+
+  //===========================================================================
+  // enum : rgba
+  //===========================================================================
+
+  /////////////////////////////////////////////////////////////////////////////
+  /// \brief One of the primary colors
+  /////////////////////////////////////////////////////////////////////////////
+  enum class rgba {
+    r,g,b,a
+  };
+
+  //===========================================================================
+  // non-member functions
+  //===========================================================================
+
+  /// \brief Gets the size of a pixel given the format
+  ///
+  /// \param format the pixel format of the image
+  /// \return the size of a given pixel color
+  inline std::size_t pixel_format_size(alloy::core::image::pixel_format format)
+    noexcept
+  {
+    using pixel_format = alloy::core::image::pixel_format;
+
+    switch (format) {
+      case pixel_format::bgr:
+        [[fallthrough]];
+      case pixel_format::rgb: {
+        return 3u;
+      }
+      case pixel_format::bgra:
+        [[fallthrough]];
+      case pixel_format::rgba: {
+        return 4u;
+      }
+    }
+    ALLOY_UNREACHABLE();
+  }
+
+  /// \brief Gets whether the input format is valid
+  ///
+  /// \param format the fomat
+  /// \return \c true if the format is correct
+  inline bool is_valid_format(alloy::core::image::pixel_format format)
+    noexcept
+  {
+    using pixel_format = alloy::core::image::pixel_format;
+
+    switch (format) {
+      case pixel_format::bgr:
+        [[fallthrough]];
+      case pixel_format::bgra:
+        [[fallthrough]];
+      case pixel_format::rgb:
+        [[fallthrough]];
+      case pixel_format::rgba: {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  using rgba_index_function = rgba(*)(std::size_t pixel);
+
+  inline rgba bgr_index(std::size_t pixel)
+  {
+    ALLOY_ASSERT(pixel < 3u);
+
+    switch (pixel) {
+      case 0: return rgba::b;
+      case 1: return rgba::g;
+      case 2: return rgba::r;
+    }
+    ALLOY_UNREACHABLE();
+  }
+
+
+  inline rgba rgba_index(std::size_t pixel)
+  {
+    ALLOY_ASSERT(pixel < 4u);
+
+    switch (pixel) {
+      case 0: return rgba::r;
+      case 1: return rgba::g;
+      case 2: return rgba::b;
+      case 3: return rgba::a;
+    }
+    ALLOY_UNREACHABLE();
+  }
+
+
+  inline rgba bgra_index(std::size_t pixel)
+  {
+    ALLOY_ASSERT(pixel < 4u);
+
+    switch (pixel) {
+      case 0: return rgba::b;
+      case 1: return rgba::g;
+      case 2: return rgba::r;
+      case 3: return rgba::a;
+    }
+    ALLOY_UNREACHABLE();
+  }
+
+  inline rgba rgb_index(std::size_t pixel)
+  {
+    ALLOY_ASSERT(pixel < 3u);
+
+    switch (pixel) {
+      case 0: return rgba::r;
+      case 1: return rgba::g;
+      case 2: return rgba::b;
+    }
+    ALLOY_UNREACHABLE();
+  }
+
+  /// \brief Gets a function to convert an integral index between 0..3 to
+  ///        the corresponding pixel color
+  ///
+  /// \param format the pixel format
+  /// \return A function pointer to a conversion function
+  inline rgba_index_function
+    get_pixel_index_function(alloy::core::image::pixel_format format)
+  {
+    using pixel_format = alloy::core::image::pixel_format;
+
+    ALLOY_ASSERT(::is_valid_format(format));
+
+    switch (format) {
+      case pixel_format::bgr: {
+        return &::bgr_index;
+      }
+      case pixel_format::rgb: {
+        return &::rgb_index;
+      }
+      case pixel_format::bgra: {
+        return &::bgra_index;
+      }
+      case pixel_format::rgba: {
+        return &::rgba_index;
+      }
+    }
+    ALLOY_UNREACHABLE();
+  }
+} // namespace <anonymous>
+
+//=============================================================================
+// class : image
+//=============================================================================
+
+//-----------------------------------------------------------------------------
+// Static Factories
+//-----------------------------------------------------------------------------
+
+alloy::core::expected<alloy::core::image>
+  alloy::core::image::make_image_from_buffer(vector<std::byte> buffer,
+                                             dimensions d,
+                                             pixel_format format)
+{
+  if (ALLOY_UNLIKELY(!::is_valid_format(format))) {
+    return unexpected(error_code::invalid_pixel_format);
+  }
+  const auto pixel_size = ::pixel_format_size(format);
+  const auto expected_size = d.width * d.height * pixel_size;
+
+  if (ALLOY_UNLIKELY(d.width == 0u)) {
+    return unexpected(error_code::bad_width);
+  }
+  if (ALLOY_UNLIKELY(d.height == 0u)) {
+    return unexpected(error_code::bad_height);
+  }
+  if (ALLOY_UNLIKELY(buffer.size() != expected_size)) {
+    return unexpected(error_code::incorrect_dimensions);
+  }
+
+  return image{std::move(buffer), d, format};
+}
+
+
+alloy::core::expected<alloy::core::image>
+  alloy::core::image::make_blank_image(dimensions d,
+                                       pixel_format format,
+                                       color background)
+{
+  return make_blank_image(d, format, background, allocator{});
+}
+
+
+alloy::core::expected<alloy::core::image>
+  alloy::core::image::make_blank_image(dimensions d,
+                                       pixel_format format,
+                                       color background,
+                                       allocator alloc)
+{
+  const auto pixel_size = pixel_format_size(format);
+  auto adapter = stl_allocator_adapter<std::byte>{alloc};
+
+  auto vec = vector<std::byte>{adapter};
+  vec.resize(d.width * d.height * pixel_size);
+
+  const auto convert = ::get_pixel_index_function(format);
+  auto pixel = std::size_t{0u};
+
+  for (auto& v : vec) {
+    const auto color_segment = (*convert)(pixel);
+
+    switch (color_segment) {
+    case rgba::r:
+      v = static_cast<std::byte>(255u * background.r());
+      break;
+    case rgba::g:
+      v = static_cast<std::byte>(255u * background.g());
+      break;
+    case rgba::b:
+      v = static_cast<std::byte>(255u * background.b());
+      break;
+    case rgba::a:
+      v = static_cast<std::byte>(255u * background.a());
+      break;
+    }
+
+    pixel = (pixel + 1u) % pixel_size;
+  }
+
+  return make_image_from_buffer(std::move(vec), d, format);
+}
+
+//-----------------------------------------------------------------------------
+// Constructors
+//-----------------------------------------------------------------------------
+
+alloy::core::image::image()
+  noexcept
+  : m_bytes{},
+    m_width{0u},
+    m_height{0u},
+    m_format{pixel_format::rgb}
+{
+
+}
+
+//-----------------------------------------------------------------------------
+// Modifiers
+//-----------------------------------------------------------------------------
+
+void alloy::core::image::set_pixel(std::size_t x, std::size_t y, color c)
+  noexcept
+{
+  ALLOY_ASSERT(x < m_width);
+  ALLOY_ASSERT(y < m_height);
+
+  const auto pixel_size = ::pixel_format_size(m_format);
+  const auto index = (y * m_width * pixel_size) + x * pixel_size;
+  const auto convert = ::get_pixel_index_function(m_format);
+
+  for (auto i = 0u; i < pixel_size; ++i) {
+    const auto color_component = (*convert)(i);
+
+    switch (color_component) {
+    case rgba::r:
+      m_bytes[index + i] = static_cast<std::byte>(255u * c.r());
+      break;
+    case rgba::g:
+      m_bytes[index + i] = static_cast<std::byte>(255u * c.g());
+      break;
+    case rgba::b:
+      m_bytes[index + i] = static_cast<std::byte>(255u * c.b());
+      break;
+    case rgba::a:
+      m_bytes[index + i] = static_cast<std::byte>(255u * c.a());
+      break;
+    }
+  }
+}
+
+//-----------------------------------------------------------------------------
+// Accessors
+//-----------------------------------------------------------------------------
+
+alloy::core::color
+  alloy::core::image::get_pixel(std::size_t x, std::size_t y)
+  const noexcept
+{
+  ALLOY_ASSERT(x < m_width);
+  ALLOY_ASSERT(y < m_height);
+
+  const auto pixel_size = ::pixel_format_size(m_format);
+  const auto index = (y * m_width * pixel_size) + x * pixel_size;
+  const auto convert = ::get_pixel_index_function(m_format);
+
+  auto result = color{};
+
+  for (auto i = 0u; i < pixel_size; ++i) {
+    const auto color_component = (*convert)(i);
+
+    switch (color_component) {
+    case rgba::r:
+      result.r() = static_cast<real>(m_bytes[index + i]) / real{255};
+      break;
+    case rgba::g:
+      result.g() = static_cast<real>(m_bytes[index + i]) / real{255};
+      break;
+    case rgba::b:
+      result.b() = static_cast<real>(m_bytes[index + i]) / real{255};
+      break;
+    case rgba::a:
+      result.a() = static_cast<real>(m_bytes[index + i]) / real{255};
+      break;
+    }
+  }
+
+  return result;
+}
+
+//-----------------------------------------------------------------------------
+// Observers
+//-----------------------------------------------------------------------------
+
+alloy::core::image alloy::core::image::copy()
+  const
+{
+  return image{
+    m_bytes.copy(),
+    dimensions{m_width, m_height},
+    m_format
+  };
+}
+
+
+alloy::core::image alloy::core::image::copy(allocator alloc)
+  const
+{
+  const auto adapter = stl_allocator_adapter<std::byte>{alloc};
+
+  auto vec = vector<std::byte>{adapter};
+  vec.reserve(m_bytes.size());
+  vec.assign(m_bytes.begin(), m_bytes.end());
+
+  return image{
+    std::move(vec),
+    dimensions{m_width, m_height},
+    m_format
+  };
+}
+
+//-----------------------------------------------------------------------------
+// Private : Constructor
+//-----------------------------------------------------------------------------
+
+alloy::core::image::image(vector<std::byte> bytes,
+                          dimensions d,
+                          pixel_format format)
+  noexcept
+  : m_bytes{std::move(bytes)},
+    m_width{d.width},
+    m_height{d.height},
+    m_format{format}
+{
+
+}
+
+//=============================================================================
+// non-member functions : enum : image::error_code
+//=============================================================================
+
+//-----------------------------------------------------------------------------
+// Utilities
+//-----------------------------------------------------------------------------
+
+std::error_code alloy::core::make_error_code(image::error_code ec)
+  noexcept
+{
+  static auto s_error_category = ::error_category{};
+
+  return std::error_code{
+    static_cast<int>(ec),
+    s_error_category
+  };
+}
