d9568a79218c4ad79dc254fbf5da66f9bc648e82
Matthew Rodusek <matthew.rodusek@gmail.com>
2019-10-03 22:49:50 -0400

    Add xor file adapter
    This new file adapter performs an XOR pass of every byte being written
    and read from the underlying file to perform a very simple form of
    obfuscation.

---
+++ lib/alloy-io/include/alloy/io/filesystem/xor_file_adapter.hpp
@@ -0,0 +1,119 @@
+/*****************************************************************************
+ * \file xor_file_adapter.hpp
+ *
+ * \brief
+ *****************************************************************************/
+
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2019 Matthew Rodusek All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+#ifndef ALLOY_IO_FILESYSTEM_XOR_FILE_ADAPTER_HPP
+#define ALLOY_IO_FILESYSTEM_XOR_FILE_ADAPTER_HPP
+
+#include "alloy/io/filesystem/file_adapter.hpp"
+#include "alloy/io/filesystem/file_stream_finalizer.hpp"
+#include "alloy/io/filesystem/file.hpp"
+#include "alloy/core/memory/allocator.hpp"
+
+#include <cstddef> // std::byte
+
+namespace alloy::io {
+
+  //===========================================================================
+  // class : xor_file_adapter
+  //===========================================================================
+
+  /////////////////////////////////////////////////////////////////////////////
+  /// \brief An interface for adapting a file into a different type of file.
+  ///
+  /// This is used to allow composable
+  /////////////////////////////////////////////////////////////////////////////
+  class xor_file_adapter final
+    : public file_adapter,
+      private file_stream_finalizer
+  {
+    //-------------------------------------------------------------------------
+    // Constructors / Assignment
+    //-------------------------------------------------------------------------
+  public:
+
+    /// \brief Constructs a file adapter that uses default values for the
+    ///        XOR pass
+    ///
+    /// This uses the default allocator
+    xor_file_adapter() noexcept;
+
+    /// \brief Constructs a file adapter that uses default values for the
+    ///        XOR pass, and is allocated using \p alloc
+    ///
+    /// \param alloc the allocator to use for creating the file adapters
+    explicit xor_file_adapter(core::allocator alloc) noexcept;
+
+    /// \brief Constructs a file adapter that runs an XOR pass of \p b0
+    ///        followed by \p b1 on each byte read (and reverses on write)
+    ///
+    /// \param b0 the first byte to xor on read (second on write)
+    /// \param b1 the second byte to xor on read (first on write)
+    xor_file_adapter(std::byte b0,
+                     std::byte b1) noexcept;
+
+    /// \brief Constructs a file adapter that runs an XOR pass of \p b0
+    ///        followed by \p b1 on each byte read (and reverses on write)
+    ///
+    /// \param b0 the first byte to xor on read (second on write)
+    /// \param b1 the second byte to xor on read (first on write)
+    /// \param alloc the allocator to use for constructing adapters
+    xor_file_adapter(std::byte b0,
+                     std::byte b1,
+                     core::allocator alloc) noexcept;
+
+    xor_file_adapter(const xor_file_adapter&) = delete;
+    xor_file_adapter(xor_file_adapter&&) = delete;
+
+    //-------------------------------------------------------------------------
+
+    xor_file_adapter& operator=(const xor_file_adapter&) = delete;
+    xor_file_adapter& operator=(xor_file_adapter&&) = delete;
+
+    //-------------------------------------------------------------------------
+    // Virtual Hooks
+    //-------------------------------------------------------------------------
+  private:
+
+    file do_adapt(file f) noexcept override;
+
+    void finalize(file_stream* stream) noexcept override;
+
+    //-------------------------------------------------------------------------
+    // Private Members
+    //-------------------------------------------------------------------------
+  private:
+
+    core::allocator m_allocator;
+    std::byte m_byte0;
+    std::byte m_byte1;
+  };
+
+} // namespace alloy::io
+
+#endif /* ALLOY_IO_FILESYSTEM_XOR_FILE_ADAPTER_HPP */
---
+++ lib/alloy-io/src/alloy/io/filesystem/xor_file_adapter.cpp
@@ -0,0 +1,269 @@
+#include "alloy/io/filesystem/xor_file_adapter.hpp"
+
+#include <array>     // std::array
+#include <algorithm> // std::min
+
+namespace {
+
+  //===========================================================================
+  // class : xor_file_stream
+  //===========================================================================
+
+  class xor_file_stream final
+    : public alloy::io::file_stream
+  {
+    //-------------------------------------------------------------------------
+    // Constructor
+    //-------------------------------------------------------------------------
+  public:
+
+    /// \brief Constructs this xor file stream from the underlying file, and
+    ///        the bytes to perform the XOR with
+    ///
+    /// \param file the file to adapt
+    /// \param b0 the first byte to XOR
+    /// \param b1 the second byte to XOR
+    explicit xor_file_stream(alloy::io::file file,
+                             std::byte b0,
+                             std::byte b1) noexcept;
+
+    //-------------------------------------------------------------------------
+    // Observers
+    //-------------------------------------------------------------------------
+  public:
+
+    /// \brief Returns the size of the file
+    ///
+    /// \return the size of the file
+    alloy::core::expected<size_type> bytes() const noexcept override;
+
+    //-------------------------------------------------------------------------
+    // File API
+    //-------------------------------------------------------------------------
+  public:
+
+    /// \brief Closes this file
+    ///
+    /// This just passes through to the adapted file
+    void close() noexcept override;
+
+    /// \brief Skips the next \p offset bytes
+    ///
+    /// This just passes through to the adapted file
+    ///
+    /// \param offset the offset to skip past
+    /// \return the result of skipping
+    alloy::core::expected<void>
+      skip(offset_type offset) noexcept override;
+
+    /// \brief Reads values from the file and XOR's the result
+    ///
+    /// \param buffer the buffer to read into
+    /// \return a buffer containing the read result
+    alloy::core::expected<alloy::io::mutable_buffer>
+      read(alloy::io::mutable_buffer buffer) noexcept override;
+
+    /// \brief Writes values into the buffer and XOR's them
+    ///
+    /// \param buffer the buffer to write
+    /// \return a buffer of the written bytes
+    alloy::core::expected<alloy::io::const_buffer>
+      write(alloy::io::const_buffer buffer) noexcept override;
+
+    //-------------------------------------------------------------------------
+    // Private Members
+    //-------------------------------------------------------------------------
+  private:
+
+    alloy::io::file m_file;
+    std::byte m_byte0;
+    std::byte m_byte1;
+  };
+
+  //===========================================================================
+  // inline definitions : class : xor_file_stream
+  //===========================================================================
+
+  //---------------------------------------------------------------------------
+  // Constructors
+  //---------------------------------------------------------------------------
+
+  xor_file_stream::xor_file_stream(alloy::io::file file,
+                                   std::byte b0,
+                                   std::byte b1)
+    noexcept
+    : m_file{std::move(file)},
+      m_byte0{b0},
+      m_byte1{b1}
+  {
+
+  }
+
+  //---------------------------------------------------------------------------
+  // Observers
+  //---------------------------------------------------------------------------
+
+  alloy::core::expected<alloy::io::file_stream::size_type>
+    xor_file_stream::bytes()
+    const noexcept
+  {
+    return m_file.bytes();
+  }
+
+  //---------------------------------------------------------------------------
+  // File API
+  //---------------------------------------------------------------------------
+
+  void xor_file_stream::close()
+    noexcept
+  {
+    m_file.close();
+  }
+
+  alloy::core::expected<void>
+    xor_file_stream::skip(offset_type offset)
+    noexcept
+  {
+    return m_file.skip(offset);
+  }
+
+  alloy::core::expected<alloy::io::mutable_buffer>
+    xor_file_stream::read(alloy::io::mutable_buffer buffer)
+    noexcept
+  {
+    auto result = m_file.read(buffer);
+    if (!result) {
+      return result;
+    }
+    auto new_buffer = result.value();
+
+    auto* const end   = new_buffer.data() + new_buffer.size();
+    auto* const begin = new_buffer.data();
+    auto* it = begin;
+
+    // Run the XOR pass
+    for (; it != end; ++it) {
+      *it ^= m_byte0;
+      *it ^= m_byte1;
+    }
+
+    return new_buffer;
+  }
+
+  alloy::core::expected<alloy::io::const_buffer>
+    xor_file_stream::write(alloy::io::const_buffer buffer)
+    noexcept
+  {
+    // Use thread-local to allow parallel files
+    static constexpr auto buffer_size = std::size_t{128u};
+    thread_local auto s_buffer = std::array<std::byte,buffer_size>{};
+
+    const auto size = buffer.size();
+    const auto* const data = buffer.data();
+
+    auto remaining = buffer.size();
+    auto i = 0u;
+
+    while (remaining > 0) {
+      const auto to_write = std::min(remaining, buffer_size);
+
+      for (auto j = 0u; j < to_write && i < size; ++i, ++j) {
+        s_buffer[j] = data[i] ^ m_byte1;
+        s_buffer[j] = data[i] ^ m_byte0;
+      }
+
+      auto new_buffer = alloy::io::const_buffer{
+        s_buffer.data(),
+        to_write
+      };
+      auto result = m_file.write(new_buffer);
+
+      if (!result) {
+        return result;
+      }
+      remaining -= to_write;
+    }
+
+    return buffer;
+  }
+} // namespace <anonymous>
+
+//=============================================================================
+// definitions : class : xor_file_adapter
+//=============================================================================
+
+//-----------------------------------------------------------------------------
+// Constructors
+//-----------------------------------------------------------------------------
+
+alloy::io::xor_file_adapter::xor_file_adapter()
+  noexcept
+  : xor_file_adapter{
+      std::byte{58u},
+      std::byte{129u},
+      core::allocator{}
+    }
+{
+
+}
+
+
+alloy::io::xor_file_adapter::xor_file_adapter(core::allocator alloc)
+  noexcept
+  : xor_file_adapter{
+      std::byte{58u},
+      std::byte{129u},
+      alloc
+    }
+{
+
+}
+
+
+alloy::io::xor_file_adapter::xor_file_adapter(std::byte b0,
+                                              std::byte b1)
+  noexcept
+  : xor_file_adapter{
+      b0,
+      b1,
+      core::allocator{}
+    }
+{
+
+}
+
+
+alloy::io::xor_file_adapter::xor_file_adapter(std::byte b0,
+                                              std::byte b1,
+                                              core::allocator alloc)
+  noexcept
+  : m_allocator{alloc},
+    m_byte0{b0},
+    m_byte1{b1}
+{
+
+}
+
+//-----------------------------------------------------------------------------
+// Virtual Hooks
+//-----------------------------------------------------------------------------
+
+alloy::io::file alloy::io::xor_file_adapter::do_adapt(file f)
+  noexcept
+{
+  auto* stream = m_allocator.make<::xor_file_stream>(
+    std::move(f),
+    m_byte0,
+    m_byte1
+  );
+
+  return file{stream, this};
+}
+
+void alloy::io::xor_file_adapter::finalize(file_stream* stream)
+  noexcept
+{
+  auto* p = static_cast<::xor_file_stream*>(stream);
+
+  m_allocator.dispose<::xor_file_stream>(p);
+}
