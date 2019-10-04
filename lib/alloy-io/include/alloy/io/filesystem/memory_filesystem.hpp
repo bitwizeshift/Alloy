a29529ef86d3e6e170cdac1875c053ea89d63746
Matthew Rodusek <matthew.rodusek@gmail.com>
2019-10-03 21:31:38 -0400

    Add memory filesystem
    This filesystem reads and writes into a memory buffer to behave like
    a file, while actually reading/writing from memory.

---
+++ lib/alloy-io/include/alloy/io/filesystem/memory_filesystem.hpp
@@ -0,0 +1,107 @@
+/*****************************************************************************
+ * \file memory_filesystem.hpp
+ *
+ * \brief This header contains the definition of a filesystem that reads and
+ *        writes to memory, rather than to an actual file.
+ *****************************************************************************/
+
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2019 Matthew Rodusek All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+#ifndef ALLOY_IO_FILESYSTEM_MEMORY_FILESYSTEM_HPP
+#define ALLOY_IO_FILESYSTEM_MEMORY_FILESYSTEM_HPP
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include "alloy/io/buffers/mutable_buffer.hpp"
+#include "alloy/io/filesystem/file.hpp"
+#include "alloy/io/filesystem/file_stream.hpp"
+#include "alloy/io/filesystem/file_stream_finalizer.hpp"
+
+#include "alloy/core/memory/allocator.hpp"
+
+namespace alloy::io {
+
+  /////////////////////////////////////////////////////////////////////////////
+  /// \brief A filesystem that reads and writes to memory.
+  ///
+  /// Files read and write from mutable buffers that are specified from
+  /// outside the system.
+  /////////////////////////////////////////////////////////////////////////////
+  class memory_filesystem final
+    : private file_stream_finalizer
+  {
+    //-------------------------------------------------------------------------
+    // Constructors / Assignment
+    //-------------------------------------------------------------------------
+  public:
+
+    /// \brief Constructs a memory filesystem using a default allocator
+    memory_filesystem() noexcept;
+
+    /// \brief Constructs a memory filesystem that uses \p alloc to allocate
+    ///        spaces for the files
+    ///
+    /// \param alloc the allocator
+    explicit memory_filesystem(core::allocator alloc) noexcept;
+    memory_filesystem(memory_filesystem&&) = delete;
+    memory_filesystem(const memory_filesystem&) = delete;
+
+    //-------------------------------------------------------------------------
+
+    memory_filesystem& operator=(memory_filesystem&&) = delete;
+    memory_filesystem& operator=(const memory_filesystem&) = delete;
+
+    //-------------------------------------------------------------------------
+    // Filesystem
+    //-------------------------------------------------------------------------
+  public:
+
+    /// \brief Opens a file that reads and writes from \p buffer
+    ///
+    /// \return a file that reads and writes from \p buffer
+    file open(mutable_buffer buffer) noexcept;
+
+    //-------------------------------------------------------------------------
+    // Virtual Hooks : Finalization
+    //-------------------------------------------------------------------------
+  public:
+
+    /// \brief Finalizes the file stream
+    ///
+    /// \param stream the stream to finalize
+    void finalize(file_stream* stream) noexcept override;
+
+    //-------------------------------------------------------------------------
+    // Private Members
+    //-------------------------------------------------------------------------
+  public:
+
+    core::allocator m_allocator;
+  };
+
+} // namespace alloy::io
+
+#endif /* ALLOY_IO_FILESYSTEM_MEMORY_FILESYSTEM_HPP */
---
+++ lib/alloy-io/src/alloy/io/filesystem/memory_filesystem.cpp
@@ -0,0 +1,242 @@
+#include "alloy/io/filesystem/memory_filesystem.hpp"
+#include "alloy/io/filesystem/file_stream.hpp"
+#include "alloy/io/filesystem/file_stream_finalizer.hpp"
+
+#include "alloy/core/intrinsics.hpp" // core::compiler::unused
+
+#include <algorithm> // std::min
+
+namespace {
+
+  //===========================================================================
+  // class : memory_file_stream
+  //===========================================================================
+
+  /////////////////////////////////////////////////////////////////////////////
+  /// \brief A memory file stream
+  ///
+  /// This file will read and write from memory
+  /////////////////////////////////////////////////////////////////////////////
+  class memory_file_stream final
+    : public alloy::io::file_stream
+  {
+    //-------------------------------------------------------------------------
+    // Constructors
+    //-------------------------------------------------------------------------
+  public:
+
+    /// \brief Constructs a memory file stream that will write into and read
+    ///        from the specified \p buffer
+    ///
+    /// \param buffer the buffer to write into and read from
+    explicit memory_file_stream(alloy::io::mutable_buffer buffer) noexcept;
+
+    //-------------------------------------------------------------------------
+    // Observers
+    //-------------------------------------------------------------------------
+  public:
+
+    /// \brief Returns the size of the mutable buffer
+    ///
+    /// \return the size of the buffer
+    alloy::core::expected<size_type> bytes() const noexcept override;
+
+    //-------------------------------------------------------------------------
+    // File API
+    //-------------------------------------------------------------------------
+  public:
+
+    /// \brief Closes this file
+    ///
+    /// This is a no-op for memory files
+    void close() noexcept override;
+
+    /// \brief Skips the next \p offset bytes from this file
+    ///
+    /// \param offset the offset to skip
+    /// \return void
+    alloy::core::expected<void>
+      skip(offset_type offset) noexcept override;
+
+    /// \brief Reads from the buffer
+    ///
+    /// \param buffer the buffer to read from
+    /// \return a buffer to what was read
+    alloy::core::expected<alloy::io::mutable_buffer>
+      read(alloy::io::mutable_buffer buffer) noexcept override;
+
+    /// \brief Writes memory from the \p buffer into the stored memory
+    ///
+    /// \param buffer the buffer to write
+    /// \return the buffer to what was written
+    alloy::core::expected<alloy::io::const_buffer>
+      write(alloy::io::const_buffer buffer) noexcept override;
+
+    //-------------------------------------------------------------------------
+    // Private Members
+    //-------------------------------------------------------------------------
+  private:
+
+    alloy::io::mutable_buffer m_buffer;
+    std::size_t m_index;
+  };
+
+  //===========================================================================
+  // inline definitions : class : memory_file_stream
+  //===========================================================================
+
+  //---------------------------------------------------------------------------
+  // Constructors
+  //---------------------------------------------------------------------------
+
+  memory_file_stream::memory_file_stream(alloy::io::mutable_buffer buffer)
+    noexcept
+    : m_buffer{buffer},
+      m_index{0u}
+  {
+
+  }
+
+  //---------------------------------------------------------------------------
+  // Observers
+  //---------------------------------------------------------------------------
+
+  alloy::core::expected<alloy::io::file_stream::size_type>
+    memory_file_stream::bytes()
+    const noexcept
+  {
+    return m_buffer.size();
+  }
+
+  //---------------------------------------------------------------------------
+  // File API
+  //---------------------------------------------------------------------------
+
+  void memory_file_stream::close()
+    noexcept
+  {
+    // Nothing to do
+  }
+
+  alloy::core::expected<void>
+    memory_file_stream::skip(offset_type offset)
+    noexcept
+  {
+    const auto next_index = std::min(m_index + offset, m_buffer.size());
+
+    if (next_index == m_index) {
+      return alloy::core::unexpected(alloy::io::file::error_code::eof);
+    }
+
+    m_index = next_index;
+
+    return {};
+  }
+
+  alloy::core::expected<alloy::io::mutable_buffer>
+    memory_file_stream::read(alloy::io::mutable_buffer buffer)
+    noexcept
+  {
+    if (m_buffer.size() == m_index) {
+      return alloy::core::unexpected(alloy::io::file::error_code::eof);
+    }
+
+    // To write to
+    auto* const end   = buffer.data() + buffer.size();
+    auto* const begin = buffer.data();
+
+    // To read from
+    auto* const data = m_buffer.data();
+    const auto size  = m_buffer.size();
+
+    const auto index_before = m_index;
+    auto* it = begin;
+
+    for (; it != end && m_index < size; ++it, ++m_index) {
+      *it = data[m_index];
+    }
+
+    const auto bytes_read = m_index - index_before;
+
+    return alloy::io::mutable_buffer{
+      buffer.data(),
+      bytes_read
+    };
+  }
+
+  alloy::core::expected<alloy::io::const_buffer>
+    memory_file_stream::write(alloy::io::const_buffer buffer)
+    noexcept
+  {
+    if (m_buffer.size() == m_index) {
+      return alloy::core::unexpected(alloy::io::file::error_code::eof);
+    }
+
+    // To read from
+    auto* const end   = buffer.data() + buffer.size();
+    auto* const begin = buffer.data();
+
+    // To write into
+    auto* const data = m_buffer.data();
+    const auto size  = m_buffer.size();
+
+    const auto index_before = m_index;
+    auto* it = begin;
+
+    for (; it != end && m_index < size; ++it, ++m_index) {
+      data[m_index] = *it;
+    }
+
+    const auto bytes_written = m_index - index_before;
+
+    return alloy::io::const_buffer{
+      buffer.data(),
+      bytes_written
+    };
+  }
+
+} // namespace <anonymous>
+
+//=============================================================================
+// definitions : class : memory_filesystem
+//=============================================================================
+
+//-----------------------------------------------------------------------------
+// Constructors
+//-----------------------------------------------------------------------------
+
+alloy::io::memory_filesystem::memory_filesystem()
+  noexcept
+  : memory_filesystem{core::allocator{}}
+{
+
+}
+
+
+alloy::io::memory_filesystem::memory_filesystem(core::allocator alloc)
+  noexcept
+  : m_allocator{alloc}
+{
+
+}
+
+//-----------------------------------------------------------------------------
+// Filesystem
+//-----------------------------------------------------------------------------
+
+alloy::io::file alloy::io::memory_filesystem::open(mutable_buffer buffer)
+  noexcept
+{
+  auto* const stream = m_allocator.make<::memory_file_stream>(buffer);
+
+  return file{stream, this};
+}
+
+
+void alloy::io::memory_filesystem::finalize(file_stream* stream)
+  noexcept
+{
+  auto* const p = static_cast<::memory_file_stream*>(stream);
+
+  m_allocator.dispose<::memory_file_stream>(p);
+}
