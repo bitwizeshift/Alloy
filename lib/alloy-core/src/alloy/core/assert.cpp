15252f5d1b2801db7dde4915cc5d584ffc7e6e61
Matthew Rodusek <matthew.rodusek@gmail.com>
2020-07-12 21:34:04 -0400

    🔨 Refactor assert handlers
    The `ALLOY_ASSERT` macro has been updated to now support a
    user-configurable assertion handler. To do this, the previously
    inlined assertion handler has now been moved to its own translation
    unit -- moving the breakpoint to be in the body of the assertion
    lambda. The actual handler itself is documented to be a cold-path
    to minimize impact on the caller so the icache doesn't get polluted.

---
+++ lib/alloy-core/src/alloy/core/assert.cpp
@@ -0,0 +1,91 @@
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2020 Matthew Rodusek All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#include "alloy/core/assert.hpp"
+
+#include <cstdio>      // std::printf
+#include <exception>   // std::terminate
+#include <atomic>      // std::atomic
+#include <cstdlib>     // std::abort
+
+namespace alloy::core {
+namespace {
+
+  [[noreturn]]
+  void default_assert_handler() noexcept
+  {
+    std::terminate();
+  }
+
+  auto s_assert_handler = std::atomic<assert_handler_fn*>{&default_assert_handler};
+
+} // namespace <anonymous>
+} // namespace alloy::core
+
+alloy::core::assert_handler_fn*
+  alloy::core::set_assert_handler(assert_handler_fn* handler)
+  noexcept
+{
+  if (handler == nullptr) {
+    handler = &default_assert_handler;
+  }
+
+  return s_assert_handler.exchange(handler);
+}
+
+[[noreturn]] ALLOY_COLD
+void alloy::core::detail::assert_internal(const char* condition,
+                                          const char* message,
+                                          const char* file_name,
+                                          std::size_t line,
+                                          const char* function_name)
+  noexcept
+{
+  if (message == nullptr) {
+    std::fprintf(stderr,
+      "[assertion] %s (%zu)::%s\n"
+      "            assertion failure: condition '%s' failed\n",
+      file_name,
+      line,
+      function_name,
+      message
+    );
+  } else {
+    std::fprintf(stderr,
+      "[assertion] %s (%zu)::%s\n"
+      "            assertion failure: condition '%s' failed with message: "
+      "'%s'\n",
+      file_name,
+      line,
+      function_name,
+      condition,
+      message
+    );
+  }
+  std::fflush(stderr);
+
+  (*s_assert_handler.load())();
+  std::abort();
+}
+
