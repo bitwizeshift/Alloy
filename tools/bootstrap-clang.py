ec758990487e0a7f9376d97704de49bd18a33434
Matthew Rodusek <matthew.rodusek@gmail.com>
2020-08-23 15:55:01 -0400

    🔧 Add specific bootstrap scripts
    As part of the bootstrapping script effort, several isolated/unique
    bootstrap scripts have been added for the most common required cases.
    
    This includes:
    
    * Building for `Xcode`,
    * Building for `Visual Studio 2019`,
    * Building for `clang` using `Ninja`, and
    * Building for `clang-cl` using `Ninja`

---
+++ tools/bootstrap-clang.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Script for bootstrapping cmake for xcode
+"""
+
+def tool_path():
+  import os
+  import pathlib
+
+  path = os.path.realpath(__file__) # ./tools/bootstrap-*.py
+  path = os.path.dirname(path)      # ./tools/
+
+  return pathlib.Path(path)
+
+if __name__ == "__main__":
+
+  import subprocess
+  import sys
+
+  command = [
+    "python3", tool_path() / "bootstrap.py",
+    *sys.argv[1:],
+    "--compiler", "clang",
+    "--generator", "Ninja",
+  ]
+
+  subprocess.run(command)
