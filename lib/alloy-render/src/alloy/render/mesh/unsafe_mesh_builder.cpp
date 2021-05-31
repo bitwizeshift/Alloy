3070a2f362cc76fdf3c7dad38867f0c5b299183b
Matthew Rodusek <matthew.rodusek@gmail.com>
2021-05-30 21:16:37 -0400

    ✨ Add `unsafe_mesh_builder`
    This adds a very simple, rough `mesh_builder` implementation that does
    not attempt to uphold any invariants. The sole purpose of this type is
    to allow for very simple building of mesh objects as part of an
    implementation of other more specialized meshes.

---
+++ lib/alloy-render/src/alloy/render/mesh/unsafe_mesh_builder.cpp
@@ -0,0 +1,286 @@
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2021 Matthew Rodusek All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#include "unsafe_mesh_builder.hpp"
+
+namespace alloy::render {
+namespace {
+
+inline
+auto compress(float x)
+  noexcept -> std::int16_t
+{
+  static constexpr auto max = std::numeric_limits<std::int16_t>::max();
+
+  return static_cast<std::int16_t>(x * max);
+}
+
+inline
+auto round_up_power_two(std::size_t v)
+  noexcept -> std::size_t
+{
+  // From bit-twiddling hacks. Rounds up to nearest power-of-two.
+  // This is only done to 2 components, since we only need to round up to
+  // '4' (2^2) in the case that 2-byte (uint16_t) values are either 1 or 3
+  // component.
+  v--;
+  v |= (v >> 1);
+  v |= (v >> 2);
+  v++;
+  return v;
+}
+
+struct point_packer
+{
+  auto pack(core::packed_buffer_writer& writer, const core::point2& p) -> void
+  {
+    writer.pack_object(p.x());
+    writer.pack_object(p.y());
+  }
+  auto pack(core::packed_buffer_writer& writer, const core::point3& p) -> void
+  {
+    writer.pack_object(p.x());
+    writer.pack_object(p.y());
+    writer.pack_object(p.z());
+  }
+};
+struct vector_packer
+{
+  auto pack(core::packed_buffer_writer& writer, const core::vector3& v) -> void
+  {
+    writer.pack_object(compress(v.x()));
+    writer.pack_object(compress(v.y()));
+    writer.pack_object(compress(v.z()));
+    writer.pack_object(std::uint16_t{});
+  }
+};
+
+} //namespace <anonymous>
+} // namespace alloy::render
+
+//------------------------------------------------------------------------------
+// Constructor
+//------------------------------------------------------------------------------
+
+alloy::render::unsafe_mesh_builder::unsafe_mesh_builder(primitive_topology topology,
+                                                        core::allocator alloc)
+  noexcept
+  : m_buffer{alloc},
+    m_writer{&m_buffer},
+    m_indices{core::stl_allocator_adapter<index_type>{alloc}},
+    m_topology{topology},
+    m_bound{},
+    m_components_per_position{0u},
+    m_components_per_uv{0u},
+    m_components_per_normal{0u},
+    m_components_per_tangent{0u},
+    m_components_per_bitangent{0u}
+{
+
+}
+
+//------------------------------------------------------------------------------
+// Capacity
+//------------------------------------------------------------------------------
+
+auto alloy::render::unsafe_mesh_builder::set_components_per_position(std::uint8_t n)
+  noexcept -> void
+{
+  m_components_per_position = n;
+}
+
+auto alloy::render::unsafe_mesh_builder::set_components_per_uv(std::uint8_t n)
+  noexcept -> void
+{
+  m_components_per_uv = n;
+}
+
+auto alloy::render::unsafe_mesh_builder::set_components_per_normal(std::uint8_t n)
+  noexcept -> void
+{
+  m_components_per_normal = n;
+}
+
+auto alloy::render::unsafe_mesh_builder::set_components_per_tangent(std::uint8_t n)
+  noexcept -> void
+{
+  m_components_per_tangent = n;
+}
+
+auto alloy::render::unsafe_mesh_builder::set_components_per_bitangent(std::uint8_t n)
+  noexcept -> void
+{
+  m_components_per_bitangent = n;
+}
+
+//------------------------------------------------------------------------------
+
+auto alloy::render::unsafe_mesh_builder::reserve_vertices(std::size_t n)
+  -> void
+{
+  const auto buffer_size = (
+    m_components_per_position * core::size_of<float>() +
+    m_components_per_uv * core::size_of<float>() +
+    round_up_power_two(m_components_per_normal) * core::size_of<std::int16_t>() +
+    round_up_power_two(m_components_per_tangent) * core::size_of<std::int16_t>() +
+    round_up_power_two(m_components_per_bitangent) * core::size_of<std::int16_t>()
+  ) * n;
+
+  reserve_bytes(buffer_size);
+}
+
+auto alloy::render::unsafe_mesh_builder::reserve_bytes(core::bytes b)
+  -> void
+{
+  m_buffer.reserve(b);
+}
+
+auto alloy::render::unsafe_mesh_builder::reserve_indices(core::quantity<index_type> indices)
+  -> void
+{
+  m_indices.reserve(indices.count());
+}
+
+//------------------------------------------------------------------------------
+// Modifiers
+//------------------------------------------------------------------------------
+
+auto alloy::render::unsafe_mesh_builder::set_bounding_box(const core::axis_aligned_box& box)
+  -> void
+{
+  m_bound = box;
+}
+
+auto alloy::render::unsafe_mesh_builder::add_position(const core::point2& p)
+  -> void
+{
+  m_writer.pack_object(p, point_packer{});
+}
+
+auto alloy::render::unsafe_mesh_builder::add_position(const core::point3& p)
+  -> void
+{
+  m_writer.pack_object(p, point_packer{});
+}
+
+auto alloy::render::unsafe_mesh_builder::add_uv(float p)
+  -> void
+{
+  m_writer.pack_object(p);
+}
+
+auto alloy::render::unsafe_mesh_builder::add_uv(const core::point2& p)
+  -> void
+{
+  m_writer.pack_object(p, point_packer{});
+}
+
+auto alloy::render::unsafe_mesh_builder::add_uv(const core::point3& p)
+  -> void
+{
+  m_writer.pack_object(p, point_packer{});
+}
+
+auto alloy::render::unsafe_mesh_builder::add_normal(const core::vector3& v)
+  -> void
+{
+  m_writer.pack_object(v, vector_packer{});
+}
+
+auto alloy::render::unsafe_mesh_builder::add_tangent(const core::vector3& v)
+  -> void
+{
+  m_writer.pack_object(v, vector_packer{});
+}
+
+auto alloy::render::unsafe_mesh_builder::add_bitangent(const core::vector3& v)
+  -> void
+{
+  m_writer.pack_object(v, vector_packer{});
+}
+
+
+auto alloy::render::unsafe_mesh_builder::add_index(index_type i)
+  -> void
+{
+  m_indices.push_back(i);
+}
+
+auto alloy::render::unsafe_mesh_builder::add_face(index_type i0,
+                                                  index_type i1,
+                                                  index_type i2)
+  -> void
+{
+  add_index(i0);
+  add_index(i1);
+  add_index(i2);
+}
+
+auto alloy::render::unsafe_mesh_builder::add_face(index_type i0,
+                                                  index_type i1,
+                                                  index_type i2,
+                                                  index_type i3) -> void
+{
+  add_face(i0, i1, i2);
+  add_face(i2, i3, i0);
+}
+
+
+//------------------------------------------------------------------------------
+// Building
+//------------------------------------------------------------------------------
+
+auto alloy::render::unsafe_mesh_builder::release()
+  && -> mesh
+{
+  auto config = mesh_config{};
+  config.vertex_data = std::move(m_buffer);
+  config.indices = std::move(m_indices);
+  config.topology = m_topology;
+  config.position_components = m_components_per_position;
+  config.uv_components = m_components_per_uv;
+  config.normal_components = m_components_per_normal;
+  config.tangent_components = m_components_per_tangent;
+  config.bitangent_components = m_components_per_bitangent;
+  config.bounding_volume = m_bound;
+
+  return mesh::from_config_unchecked(std::move(config));
+}
+
+auto alloy::render::unsafe_mesh_builder::build_with(core::allocator alloc)
+    const -> mesh
+{
+  auto config = mesh_config{};
+  config.vertex_data = m_buffer.copy_with(alloc);
+  config.indices = m_indices.copy_with(core::stl_allocator_adapter<index_type>{alloc});
+  config.topology = m_topology;
+  config.position_components = m_components_per_position;
+  config.uv_components = m_components_per_uv;
+  config.normal_components = m_components_per_normal;
+  config.tangent_components = m_components_per_tangent;
+  config.bitangent_components = m_components_per_bitangent;
+  config.bounding_volume = m_bound;
+
+  return mesh::from_config_unchecked(std::move(config));
+}
---
+++ lib/alloy-render/src/alloy/render/mesh/unsafe_mesh_builder.hpp
@@ -0,0 +1,152 @@
+////////////////////////////////////////////////////////////////////////////////
+/// \file unsafe_mesh_builder.hpp .hpp
+///
+/// \brief TODO(Bitwize): Add description
+////////////////////////////////////////////////////////////////////////////////
+
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2021 Matthew Rodusek All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#ifndef SRC_ALLOY_RENDER_MESH_UNSAFE_MESH_BUILDER_HPP
+#define SRC_ALLOY_RENDER_MESH_UNSAFE_MESH_BUILDER_HPP
+
+#include "alloy/render/mesh/mesh_builder.hpp"
+#include "alloy/render/mesh/mesh.hpp"
+#include "alloy/render/mesh/primitive_topology.hpp"
+
+#include "alloy/core/utilities/quantity.hpp"
+#include "alloy/core/geometry/point/point2.hpp"
+#include "alloy/core/geometry/point/point3.hpp"
+#include "alloy/core/math/vector/vector2.hpp"
+#include "alloy/core/math/vector/vector3.hpp"
+#include "alloy/core/memory/packed_buffer.hpp"
+#include "alloy/core/memory/allocator.hpp"
+#include "alloy/core/memory/data_quantity.hpp"
+#include "alloy/core/containers/vector.hpp"
+
+#include <cstddef>
+#include <cstdint>
+
+namespace alloy::render {
+
+  //////////////////////////////////////////////////////////////////////////////
+  /// \brief A builder for meshes that performs no safety / accuracy checks.
+  ///
+  /// This builder is essentially a wrapper around the packed_buffer writer
+  /// that produces the buffers used in meshes. This has a very weak guarantee
+  /// that all entries must be performed in the following interlaced order:
+  ///
+  /// * position,
+  /// * uv,
+  /// * normal,
+  /// * tangent,
+  /// * bitangent
+  ///
+  /// This is a private type for a reason; use at your own risk.
+  //////////////////////////////////////////////////////////////////////////////
+  class unsafe_mesh_builder : public mesh_builder
+  {
+    //--------------------------------------------------------------------------
+    // Public Member Types
+    //--------------------------------------------------------------------------
+  public:
+
+    using index_type = std::uint16_t;
+
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+  public:
+
+    unsafe_mesh_builder(
+      primitive_topology topology = primitive_topology::triangles,
+      core::allocator alloc = core::allocator{}
+    ) noexcept;
+
+    //--------------------------------------------------------------------------
+    // Capacity
+    //--------------------------------------------------------------------------
+  public:
+
+    auto set_components_per_position(std::uint8_t n) noexcept-> void;
+    auto set_components_per_uv(std::uint8_t n) noexcept-> void;
+    auto set_components_per_normal(std::uint8_t n) noexcept-> void;
+    auto set_components_per_tangent(std::uint8_t n) noexcept-> void;
+    auto set_components_per_bitangent(std::uint8_t n) noexcept-> void;
+
+    auto reserve_vertices(std::size_t n) -> void;
+    auto reserve_bytes(core::bytes b) -> void;
+    auto reserve_indices(core::quantity<index_type> indices) -> void;
+
+    //--------------------------------------------------------------------------
+    // Modifiers
+    //--------------------------------------------------------------------------
+  public:
+
+    auto set_bounding_box(const core::axis_aligned_box& box) -> void;
+
+    auto add_position(const core::point2& p) -> void;
+    auto add_position(const core::point3& p) -> void;
+
+    auto add_uv(float p) -> void;
+    auto add_uv(const core::point2& p) -> void;
+    auto add_uv(const core::point3& p) -> void;
+
+    auto add_normal(const core::vector3& v) -> void;
+    auto add_tangent(const core::vector3& v) -> void;
+    auto add_bitangent(const core::vector3& v) -> void;
+
+    auto add_index(index_type i) -> void;
+    auto add_face(index_type i0, index_type i1, index_type i2) -> void;
+    auto add_face(index_type i0, index_type i1, index_type i2, index_type i3) -> void;
+
+    //--------------------------------------------------------------------------
+    // Building
+    //--------------------------------------------------------------------------
+  public:
+
+    auto release() && -> mesh;
+
+    auto build_with(core::allocator alloc) const -> mesh override;
+
+    //--------------------------------------------------------------------------
+    // Private Members
+    //--------------------------------------------------------------------------
+  private:
+
+    core::packed_buffer m_buffer;
+    core::packed_buffer_writer m_writer;
+    core::vector<index_type> m_indices;
+    primitive_topology m_topology;
+    core::axis_aligned_box m_bound;
+    std::uint8_t m_components_per_position;
+    std::uint8_t m_components_per_uv;
+    std::uint8_t m_components_per_normal;
+    std::uint8_t m_components_per_tangent;
+    std::uint8_t m_components_per_bitangent;
+  };
+
+} // namespace alloy::render
+
+#endif /* SRC_ALLOY_RENDER_MESH_UNSAFE_MESH_BUILDER_HPP */
