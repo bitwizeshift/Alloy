d51ed1290837422be28641118e69d647c4921667
Matthew Rodusek <matthew.rodusek@gmail.com>
2021-06-06 21:49:39 -0400

    ✨ Add `ctor_function` utility
    C++17 introduced the concept of guaranteed copy-elision -- something
    which subtly blurred the lines between a function and a constructor.
    Functions that return objects by-value are now guaranteed to elide the
    copy or move -- which means that by-value functions can return
    uncopyable/unmovable objects without incurring any penalty over the
    equivalent code of writing the constructor call itself.
    
    This lends itself to some extremely beneficial patterns, since we can
    now use these "functions" to approximate a constructor (something
    previously impossible), which means we can write utilities that can be
    passed to generic callables or `std::function`s easily.
    
    This adds exactly that utility, which composes itself cleanly into
    the various monadic functionalities in Alloy.

---
+++ lib/alloy-core/include/alloy/core/utilities/ctor_function.hpp
@@ -0,0 +1,78 @@
+////////////////////////////////////////////////////////////////////////////////
+/// \file ctor_function.hpp
+///
+/// \brief TODO(Bitwize): Add description
+////////////////////////////////////////////////////////////////////////////////
+
+/*
+  The MIT License (MIT)
+
+  Copyright (c) 2021 Matthew Rodusek All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
+
+#ifndef ALLOY_CORE_UTILITIES_CTOR_FUNCTION_HPP
+#define ALLOY_CORE_UTILITIES_CTOR_FUNCTION_HPP
+
+#include "alloy/core/intrinsics.hpp"
+
+#include <type_traits> // std::enable_if
+#include <utility>     // std::forward
+
+namespace alloy::core {
+
+  //////////////////////////////////////////////////////////////////////////////
+  /// \brief A utility type for representing a constructor invocation as a
+  ///        callable object.
+  ///
+  /// C++17 introduced guaranteed-copy-elision, which ensures that you can
+  /// return a newly-constructed type even if it does not offer a copy or move
+  /// constructor. This effectively makes the distinction between what a
+  /// constructor does, and what a function does, transparent -- since the two
+  /// will be viable in the same conditions, and are required to generate the
+  /// same assembly.
+  ///
+  /// This utility makes use of this fact, with the intended purpose of
+  /// being used in composable functionalities -- such as being used as the
+  /// input to monadic functions like `foo.map(ctor_function<bar>{})` to convert
+  /// a `result<foo,E>` into a `result<bar,E>`.
+  ///
+  /// \tparam T the type to construct
+  //////////////////////////////////////////////////////////////////////////////
+  template <typename T>
+  struct ctor_function
+  {
+    template <typename...Args, typename=std::enable_if_t<std::is_constructible_v<T,Args...>>>
+    constexpr auto operator()(Args&&...args)
+      noexcept(std::is_nothrow_constructible_v<T,Args...>) -> T;
+  };
+
+} // namespace alloy::core
+
+template <typename T>
+template <typename... Args, typename>
+ALLOY_FORCE_INLINE constexpr
+auto alloy::core::ctor_function<T>::operator()(Args&&...args)
+  noexcept(std::is_nothrow_constructible_v<T, Args...>) -> T
+{
+  return T(std::forward<Args>(args)...);
+}
+
+#endif /* ALLOY_CORE_UTILITIES_CTOR_FUNCTION_HPP */
