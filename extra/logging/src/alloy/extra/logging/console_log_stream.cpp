7593bfa84bd392f7bcf627b5c6685f37d45eec39
Matthew Rodusek <matthew.rodusek@gmail.com>
2021-10-10 01:06:33 -0400

    ✨ Add color support for console logging
    The console logger has been given support for ANSI colored output when
    working in an environment with `__linux__` defined, since enabling
    ANSI support on Windows is a hassle. This may be done in the future.

--- extra/logging/src/alloy/extra/logging/console_log_stream.cpp
+++ extra/logging/src/alloy/extra/logging/console_log_stream.cpp
@@ -5,13 +5,59 @@
 #include <ctime>  // std::localtime
 #include <cstdio> // std::printf
 
+namespace alloy::extra {
 namespace {
 
-  std::string_view to_string(alloy::extra::log_level level)
-    noexcept
+  /// \brief Gets a reset code. On *nix systems, this gets the ANSI code. On
+  ///        Windows, this is empty.
+  ///
+  /// \return the code
+  constexpr auto get_reset()
+    noexcept -> const char*
   {
-    using log_level = alloy::extra::log_level;
+#if defined(__linux__)
+    return "\u001b[0m";
+#else
+    return "";
+#endif
+  }
 
+  /// \brief Converts the log \p level into an ANSI color code to use for logging
+  ///
+  /// On *nix this returns the appropriate color code, on Windows this returns
+  /// an empty string
+  ///
+  /// \return the code
+  auto to_color_code(log_level level)
+    noexcept -> const char*
+  {
+#if defined(__linux__)
+    switch (level) {
+      case log_level::debug: {
+        return "\u001b[96m";
+      }
+      case log_level::info: {
+        return "\u001b[92m";
+      }
+      case log_level::warning: {
+        return "\u001b[93m";
+      }
+      case log_level::error: {
+        return "\u001b[91m";
+      }
+      case log_level::fatal: {
+        return "\u001b[97;41m";
+      }
+    }
+    return "";
+#else
+    return "";
+#endif
+  }
+
+  auto to_string(log_level level)
+    noexcept -> std::string_view
+  {
     switch (level) {
       case log_level::debug: {
         return "debug";
@@ -33,6 +79,7 @@ namespace {
     return "unknown";
   }
 } // namespace <anonymous>
+} // namespace alloy::extra
 
 auto alloy::extra::console_log_stream::on_log_message(
   std::chrono::system_clock::time_point time,
@@ -40,6 +87,8 @@ auto alloy::extra::console_log_stream::on_log_message(
   std::string_view message
 ) -> void
 {
+  static constexpr auto reset_code = get_reset();
+
   const auto t = std::chrono::system_clock::to_time_t(time);
   const auto tm = *std::localtime(&t);
   auto buffer = std::array<char,32>{};
@@ -51,17 +100,20 @@ auto alloy::extra::console_log_stream::on_log_message(
     &tm
   );
   const auto time_string = std::string_view{buffer.data(), bytes};
-  const auto level_string = ::to_string(level);
+  const auto level_string = to_string(level);
+  const auto* const color_code = to_color_code(level);
 
   // print format is:
   // time | level | message
   std::printf(
-    "%.*s | %-7s | %.*s\n",
+    "%s%.*s | %-7s | %.*s%s\n",
+    color_code,
     static_cast<int>(time_string.size()),
     time_string.data(),
     level_string.data(),
     static_cast<int>(message.size()),
-    message.data()
+    message.data(),
+    reset_code
   );
   std::fflush(stdout);
 }
